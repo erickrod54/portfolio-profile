324bb9f53dfa4b7e145264361bcabee019c20eeb
Erick Rodriguez <erickrod54@gmail.com>
2021-10-01 07:54:24 -0400

    clearing jsx elements in App.js file and defaults import react logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Component } from 'react';
 
@@ -16,26 +16,8 @@ constructor(){
   }
 render() {
   return(
-    <div className="App">
-      {/**attributtes like className, and OnClick are JSX attributes that mimick html attributes*/}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.message}
-        </p>
-        {/**this is a test for state manipulation
-         * we change the value of p tag using a state
-         */}
-         <button onClick={() => this.setState({ message: 'doing click changes the prop string of the state'})}>Change text</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello my name is Erick - this is the start on my portfolio</p>
     </div>
   );
 }
