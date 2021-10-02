33c770493eae9883255c8718b7f957cdde9a1001
Erick Rodriguez <erickrod54@gmail.com>
2021-10-02 07:13:50 -0400

    creation of homePage componet and homepage styles, is the drawing to start to architect the portfolio

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+     <div className="cardprofile"> 
+      <p className="title">here is going to be 
+         displayed the cardProfile 
+         component for the moment we will draw it
+         for style propuses</p>
+     </div> 
+
+  </div>
+);
+
+export default HomePage;
