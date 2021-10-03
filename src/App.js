38210328dc6c2e5b3cbc58406a53dbd47b0b8927
Erick Rodriguez <erickrod54@gmail.com>
2021-10-03 08:35:39 -0400

    importing Route component from react-router-dom and making a test component called DashBoard to test two Routes /homepage and /dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
-
-
 import HomePage from './pages/homepage/homepage.component';
+
+const DashBoard = () => (
+  <div>
+    <h1>Dashboard Page</h1>
+  </div>
+);
 
 function App(){
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/dashboard' component={DashBoard}/>
     </div>
   );
 }
