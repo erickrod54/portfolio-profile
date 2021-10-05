494285ec012add03f9bad0a0a15ac16d904f6c9d
Erick Rodriguez <erickrod54@gmail.com>
2021-10-05 14:15:46 -0400

    creating sign-in page, with SignInPage component and its styles

---
+++ src/pages/sign-in-page/sign-in-page.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './sign-in-page.styles.scss'
+
+const SignInPage = () => (
+  <div className="sign-in">
+    SIGN IN
+  </div>
+);
+
+export default SignInPage;
