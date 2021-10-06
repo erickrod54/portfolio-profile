40b119ebff31b4a6653f4e29ec102f3e06ddd725
Erick Rodriguez <erickrod54@gmail.com>
2021-10-06 06:24:40 -0400

    adding sign-in.component.jsx, but still working on it

--- src/pages/sign-in-page/sign-in-page.component.jsx
+++ src/pages/sign-in-page/sign-in-page.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import SignIn from "../../components/sign-in/sign-in.component";
+
 import './sign-in-page.styles.scss'
 
 const SignInPage = () => (
   <div className="sign-in">
-    SIGN IN
+    <SignIn />
   </div>
 );
 
