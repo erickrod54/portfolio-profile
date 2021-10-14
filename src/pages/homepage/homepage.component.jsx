ca47906a041457d2471f0e52ca5995ffc7cbf7c6
Erick Rodriguez <erickrod54@gmail.com>
2021-10-14 04:55:36 -0400

    taking out the console log that show dynamic route props, i leave it in comments

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,12 +7,14 @@ import './homepage.styles.scss';
  * location, and match to build a dynamic 
  * link to a child component of homepage
  * 
+ * const HomePage = (props) => {
+   console.log(props)  
+ * 
  * example
  * 
  *  ${props.match.url}/dashboad
 */
-const HomePage = (props) => {
-console.log(props)   
+const HomePage = () => {
 return(
   <div className="homepage">
      <div className="cardprofile"> 
