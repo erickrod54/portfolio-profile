4e1f25750e79f4a538372d8eb854607115d4eeee
Erick Rodriguez <erickrod54@gmail.com>
2021-11-04 09:29:26 -0400

    adding Provider component to wrap the app, aand importing the store to use it into the provider to provide states

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider  } from 'react-redux';
 import './index.css';
 import App from './App';
+
+import store from './redux/store';
+/**store is where is the middleware where we store
+ * the root-reducer (is where the state gonna live)
+ */
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+<Provider store={store}>{/**i wrap around the whole app
+ *inside this store object called Provider from react redux
+ to provide all the things related to the store and state
+ acorss the app*/}  
 <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-</BrowserRouter>,
+</BrowserRouter>
+</Provider>,
 document.getElementById('root')
 );
 
