8fbd703af82e75f7bc6317cee2599f527a7b186a
Erick Rodriguez <erickrod54@gmail.com>
2021-11-13 11:31:39 -0500

    this file contain actions defined and to be fired by the user

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,9 @@
+/**this file manage actions for the user, for now 
+ * only to fire 'SET_CURRENT_USER' that gives shape
+ * the data() that we need for user
+ */
 export const setCurrentUser = user => ({
   type: 'SET_CURRENT_USER',
-  payload: user
+  payload:  user
 });
 
