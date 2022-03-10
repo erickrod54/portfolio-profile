cc7f46718e58395d121bc4a76742d636eb855f87
Erick Rodriguez <erickrod54@gmail.com>
2022-03-09 19:55:33 -0500

    Portfolio version 1 - CardProfile Component - Feature:building a basic jsx to create the profile spaces

---
+++ src/components/card-profile/card-profile.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+/**Portfolio version 1 - CardProfile component - Features:
+ *       --->building a basic jsx to create  
+ *           the profile spaces
+ */
+const CardProfile = () => {
+    return(
+       <>
+      <div >
+         <div >
+          <h1>this is CardProfile Component</h1>
+            <li>
+               <ul>profile 1</ul>
+            </li>
+            <li>
+               <ul>profile 2</ul>
+            </li>
+            <li>
+               <ul>profile 3</ul>
+            </li>
+         </div>
+      </div>
+       </>
+       );
+    };
+
+export default CardProfile;
