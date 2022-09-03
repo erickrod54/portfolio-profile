c3b4358addac1f385e9da8b0eb01b82c67410387
Erick Rodriguez <erickrod54@gmail.com>
2022-09-03 05:07:03 -0400

    Portfolio-erick - version 1 - App js file - Features: Building the routing, Importing and Placing HomePage, DashBoard and ResumePage Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 import HomePage from "./pages/home.page";
+import ResumePage from "./pages/resume.page";
 import DashBoard from "./pages/dashboard.page";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+/**Portfolio-erick - version 1 - App  js file -
+ * Features:
+ * 
+ *      --> Building the routing. 
+ * 
+ *      --> Importing and placing 'HomePage', 
+ *          'Dashboard' and 'ResumePage' 
+ *          Components 
+ * 
+ * Notes: By this version has been implemented
+ * the routing using react-router v6
+ */
 
 function App() {
   return (
@@ -9,6 +23,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage/>}/>
           <Route path='/dashboard' element={<DashBoard />}/>
+          <Route path='/resume' element={<ResumePage />}/>
         </Routes>
       </Router>
   );
