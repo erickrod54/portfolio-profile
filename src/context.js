96ad4891f369fc8cd7bd1a954d58e6cfba160c79
Erick Rodriguez <erickrod54@gmail.com>
2022-09-03 05:18:52 -0400

    Portfolio-erick - version 1 - context js - Features: Importing NavBarData, ResumeData, and social data from data js file, Providing the data throught the Provider

--- src/context.js
+++ src/context.js
@@ -1,12 +1,33 @@
 import React, { useContext } from "react";
-import { NavBarData } from './data'
+import { NavBarData,
+         ResumeData,
+        social   } from './data';
+
+/**Portfolio-erick - version 1 - context js -
+ * Features:
+ * 
+ *      -->  Importing 'NavBarData', 'ResumeData', 
+ *          and 'social' from data js file
+ * 
+ *      --> Providing the datta througth the provider
+ *          to use it on the application.
+ * 
+ * Notes: By this version changes to the data
+ * has been made in order to get the essential
+ * information
+ */
 
 export const AppContext = React.createContext()
 
 export const AppProvider = ({ children }) => {
 
     return(
-        <AppContext.Provider value={{ NavBarData}}>
+        <AppContext.Provider 
+            value={{ 
+                NavBarData,
+                ResumeData,
+                social
+                }}>
             {children}
         </AppContext.Provider>
     )
