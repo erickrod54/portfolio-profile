5377c90d6c53f15d5896c2783fe7ac87480dcfb7
Erick Rodriguez <erickrod54@gmail.com>
2022-09-04 00:19:57 -0400

    Portfolio-erick - version 1 - Education Component - Features: Building Education Component, Destructuring ResumeData from useAppContext(), Importing and placing EducationData Component to spread the data for the education section, Importing NavBarWrapper to extending it in EducationWrapper

---
+++ src/components/education.component.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { NavbarWrapper } from "./navbar.component";
+
+import EducationData from "./education.data.component";
+
+import { useAppContext } from '../context'
+
+/**Portfolio-erick - version 1 - Education Component -
+ * Features:
+ * 
+ *      --> Building 'Education' Component.
+ * 
+ *      --> Destructuring 'ResumeData' from 
+ *          'useAppContext()'
+ * 
+ *      --> Importing and placing 'EducationData'
+ *          Component to spread the data for 
+ *          the education section.
+ * 
+ *       --> Importing 'NavbarWrapper' to extending
+ *          it in 'EducationWrapper'.
+ * 
+ * Notes: By this version changes to the data
+ * has been made in order to get the essential
+ * information
+ */
+
+
+const Education = () => {
+
+    const { ResumeData } = useAppContext()
+
+    //console.log('from education component ==>', ResumeData)
+
+    return(
+       <EducationWrapper>
+            <h2>Education</h2>
+           {ResumeData.filter((section) => section.id === 4).map((education) => {
+            const { id } = education
+
+            //console.log(education.education)
+            return(
+                <EducationData key={id} { ...education}/>
+            )
+           })}
+       </EducationWrapper>
+    )
+}
+
+const EducationWrapper = styled(NavbarWrapper)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export default Education;
