3bc56b66d2a50fdc8221150ee7db6187ef711e55
Erick Rodriguez <erickrod54@gmail.com>
2022-09-04 00:39:10 -0400

    Portfolio-erick - version 1 - context js - Features: Importing NavBarData, ResumeData, and social from data js, Providing the data throught the provider to use it in the application, Building isModalOpen, openModal, and closeModal to build the Contact Component

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NavBarData,
          ResumeData,
         social   } from './data';
@@ -12,21 +12,41 @@ import { NavBarData,
  *      --> Providing the datta througth the provider
  *          to use it on the application.
  * 
+ *      --> Building 'isModalOpen', 'openModal' and
+ *          'closeModal' to build the Contact
+ *           Component
+ * 
  * Notes: By this version changes to the data
  * has been made in order to get the essential
  * information
+ * 
+ *  isModalOpen
+ *  closeModal
  */
 
 export const AppContext = React.createContext()
 
 export const AppProvider = ({ children }) => {
 
+    const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
     return(
         <AppContext.Provider 
             value={{ 
                 NavBarData,
                 ResumeData,
-                social
+                social,
+                isModalOpen,
+                openModal,
+                closeModal
                 }}>
             {children}
         </AppContext.Provider>
