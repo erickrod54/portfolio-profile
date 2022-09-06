d0458f396298d41fa676ecaf3d3e9b2dcb108272
Erick Rodriguez <erickrod54@gmail.com>
2022-09-06 01:15:47 -0400

    Portfolio-erick - version 3 - NavLinks - Features: Drilling openModal to trigger a modal to Contact Component

--- src/components/navlinks.component.jsx
+++ src/components/navlinks.component.jsx
@@ -1,23 +1,39 @@
 import React from "react";
+import styled from "styled-components";
 
-/**Portfolio-erick - version 1 - NavLinks - Features:
+/**Portfolio-erick - version 3 - NavLinks - Features:
  * 
- *      --> Destructuring props 'id', 'urlname' previusly
- *          drilled from 'NavBar' Component
+ *      --> Drilling 'openModal' to trigger 
+ *          a modal to contact.
  * 
- * Notes: In next version i'll style the links and keep
- * adding as they are needed.
+ * Notes: By this version all tasks are done 
+ * for this component
  */
 
-const NavLinks = ({ id, urlname }) => {
+const NavLinks = ({ id, urlname, openModal }) => {
 
     return(
+        <NavLinksWrapper>
         <ul key={id}>
-                       <li>
+                       <li className="btn" onClick={ urlname === 'contact' ? openModal : null}>
                         {urlname} 
                        </li>     
         </ul>
+        </NavLinksWrapper>
     )
 }
+
+export const NavLinksWrapper = styled.div`
+    .btn{
+        border: 1px solid var(--clr-black);
+        background: var(--clr-white);
+        color: var(--clr-black);
+    }
+
+    .btn:hover {
+     background: var(--clr-primary-5);
+     color: var(--clr-white);
+    }
+`
 
 export default NavLinks;
