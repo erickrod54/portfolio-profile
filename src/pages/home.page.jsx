4052ef728240a5272718081ae92b2c27b5afc150
Erick Rodriguez <erickrod54@gmail.com>
2022-09-06 01:10:38 -0400

    Portfolio-erick - version 2 - HomePage Component - Features: Importing Contact in order to render it and visualize it

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
 import NavBar from "../components/navbar.component";
-//import Resume from "../components/resume.component";
-//import Footer from "../components/footer.component";
+import Resume from "../components/resume.component";
+import Footer from "../components/footer.component";
 
-//import styled from "styled-components";
+import Contact from "../components/contact.component";
+
+/**Portfolio-erick - version 2 - HomePage Component -
+ * Features:
+ * 
+ *      --> Importing 'Contact' in order to render
+ *          it and visualize it.  
+ * 
+ * Notes: Changes can be made later for this Component
+ */
 
 const HomePage = () => {
 
     return(
         <>
         <NavBar />
-        {/**<Resume />
-        <Footer /> */}
+        <Resume />
+        <Footer />
+        <Contact />
         </>
     )
 }
