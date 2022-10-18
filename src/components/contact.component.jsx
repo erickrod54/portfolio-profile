55bb77a152a42e3ef5bf2d5fcbbc28560b8b1ddd
Erick Rodriguez <erickrod54@gmail.com>
2022-10-18 12:05:00 -0400

    Portfolio-erick - version 4 - Contact - Features: Building senEmail feature, Adiing closeModal in order to close email modal

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -1,35 +1,75 @@
-import React from "react";
+import React, { useRef } from "react";
 import Modal from "./modal.component";
 
 import { useAppContext } from "../context";
 
 import styled from "styled-components";
 
-/**Portfolio-erick - version 1 - Contact - Features:
+import emailjs from '@emailjs/browser';
+import { FaTimes } from 'react-icons/fa';
+
+
+/**Portfolio-erick - version 4 - Contact - Features:
  * 
- *      --> Building 'Contact' Modal.
+ *      --> Building 'sendEmail' feature.
+ * 
+ *      --> Adding closeModal in order to close 
+ *          the email modal.
  * 
  * Notes: By this version all tasks are done 
  * for this component
+ * 
+ *  name - email - subject - message
+ * 
+ * My Default Template
+Template ID:
  */
 
 const Contact = () => {
 
     const { closeModal } = useAppContext()
+    
+    const form = useRef();
 
-    return(
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_2ty4p3i', 'template_w3jpd4s', form.current, 'rQysZwpxWQqwyMhIY')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+        closeModal()
+    };
+
+    return (
         <Modal>
-            <ContactWrapper>
-            <h4>subject:</h4>
-            <input  type='text' placeholder='type subject here'/>
-            <h4>message:</h4>
-            <input className="input-message" type='text' placeholder='type message here'/>
-            <button type="submit" className="btn close-btn"
-                     onClick={closeModal }   
-                    >send message</button>
-            </ContactWrapper>
+        <button onClick={() => closeModal()} className='close-modal-btn'>
+            <FaTimes />     
+        </button>    
+        <ContactWrapper>
+        <form ref={form} onSubmit={sendEmail}>
+          <ul>
+            <label>your name</label>
+            <li>
+            <input type="text" name="user_name" placeholder='type your name'/>
+            </li>
+            <label>your email</label>
+            <li>
+            <input type="email" name="user_email" placeholder='type your email'/>
+            </li>
+            <label>Message</label>
+            <li>
+            <textarea name="message" placeholder='type your message'/>
+            <input type="submit" value="Send" />
+            </li>
+          </ul>
+        </form>
+        </ContactWrapper>    
         </Modal>
-    )
+      );
 }
 
 export const ContactWrapper = styled.div`
@@ -37,19 +77,29 @@ export const ContactWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
 
-    h4{
+    label{
         text-transform:capitalize;
-        text-decoration: underline;
-        display: flex;
+        font-weight: 700;
     }
 
-    input{
+    input, textarea{
         width: 70%;
         border-radius: .25rem;
+        resize: vertical;
     }
 
     .input-message{
         height: 4rem;
+    }
+
+    ul{
+        display: flex;
+        flex-direction: column;
+        list-style-type: none;
+    }
+
+    li{
+        padding: .60rem;
     }
 `
 
