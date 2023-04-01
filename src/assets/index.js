0dfeebdfd004cff5868f8f4b05fba3e4046dbd1b
Erick Rodriguez <erickrod54@gmail.com>
2023-04-01 17:46:08 -0400

    Portfolio-erick - version 6.04 - index assets js - Features: Importing and exporting construction background,  Changing versioning to two digits

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,17 @@
 import logo from './logo-letters.svg'
+import construction from './under-construction.jpg'
 
-export { logo }
+/**Portfolio-erick - version 6.04 - index assets js -
+ * Features:
+ * 
+ *      --> Importing and exporting construction
+ *          background.
+ * 
+ *      --> Changing versioning to two digits.
+ * 
+ * Notes: By this version changes to the data
+ * has been made in order to get the essential
+ * information and email feature
+ */
+
+export { logo, construction }
