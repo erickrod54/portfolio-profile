5e36eceb9a953d7f2209c55c7b31ab1b3dab44f3
Erick Rodriguez <erickrod54@gmail.com>
2023-04-01 17:47:15 -0400

    Portfolio-erick - version 6.04 - Logo Component - Features: Migrating Logo styles

--- src/components/logo.component.jsx
+++ src/components/logo.component.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import { logo } from "../assets/index.js";
 import styled from "styled-components";
 
-/**Portfolio-erick - version 1 - 'Logo' Component -
+/**Portfolio-erick - version 6.04 - 'Logo' Component -
  * Features:
  * 
- *      -->Importing and placing 'logo' from
- *         assets > index.
- * 
- *      --> Placing logo in an image tag and 
- *          wrapping it to style it.  
+ *      --> Migrating Logo styles.  
  * 
  * Notes: The 'logo' will be use later to
  * implement other changes.
@@ -19,33 +15,13 @@ const Logo = () => {
 
     return(
         <LogoWrapper>
-        <div>
            <img src={logo} alt='logo'/>
-        </div>
-
         </LogoWrapper>
     )
 }
 
 const LogoWrapper = styled.div`
-    img{
-        width: 7rem;
-        height: 7rem;
-    }
-
-    @media screen and (max-width: 2500px) {
-        img{
-            width: 14rem;
-            height: 14rem;
-        }
-    }
-
-    @media screen and (max-width: 800px) {
-        img{
-            width: 10rem;
-            height: 10rem;
-        }
-    }
+    
 `
 
 export default Logo;
