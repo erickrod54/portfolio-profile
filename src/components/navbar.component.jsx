f3665324f86b500482e7c799332e7d71dfabeb4d
Erick Rodriguez <erickrod54@gmail.com>
2023-03-31 23:39:17 -0400

    Portfolio-erick - version 6.01 - NavBar - Features: Migrating Style Component global styles to start to start to fix NavBar Styles

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -7,11 +7,14 @@ import styled from "styled-components";
 
 import { useAppContext } from '../context'
 
-/**Portfolio-erick - version 5 - NavBar -
+/**Portfolio-erick - version 6.02 - NavBar -
  * Features:
  * 
- *      --> Fixing resposivess for tne
- *          contact button.  
+ *      --> Migrating Style Component global styles 
+ *          to start to fix header styles.  
+ * 
+ *      --> Changing style component target from 
+ *          div to semantinc tag section.
  * 
  * Notes: In next version i'll style the links and keep
  * adding as they are needed.
@@ -25,7 +28,7 @@ const NavBar = () => {
     //console.log(NavBarData)
 
     return(
-        <NavbarWrapper>
+        <NavbarWrapper id='navbar'>
             <div className="logo">
             <Logo />
             </div>
@@ -43,72 +46,8 @@ const NavBar = () => {
         )
 }
 
-export const NavbarWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin: 2rem;
-    padding:2rem ;
-    border-radius: 2rem;
-    background: var(--gradient-bkgd);
-    color: var(--clr-white);
-    border: 1px solid black;
-
-    .nav-links{
-        width: 70%;
-        display: flex;
-        justify-content: flex-end ;
-        margin: 2rem;
-        
-
-        li{
-            list-style-type: none;
-        }
-    }
-
-    @media screen and (max-width: 800px) {
-        
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-
-        .logo{
-            
-            display:flex;
-            justify-content: center;
-        }
-        .nav-links{
-            
-            display: flex;
-            padding: 1rem;
-            flex-direction: column;
-            
-            li{
-                display: flex;
-                justify-content: center;
-                width: 80%;
-            }
-        }
-    }
-
-    @media screen and (max-width: 400px) {
-        display: flex;
-        width: 75%;
-        padding: .5rem;
-        
-        .nav-links{
-            margin-right:-42rem;
-            border: 1px solid white;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            
-            li{
-                display: flex;
-                justify-content: center;
-                width: 30%;
-            }
-        }
-    }
+export const NavbarWrapper = styled.section`
+    
 `
 
 export default NavBar;
