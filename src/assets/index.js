22c8d79e63d2a2a88464c4918bc0fc893d93c6c9
Erick Rodriguez <erickrod54@gmail.com>
2023-04-04 12:26:47 -0400

    Portfolio-erick - version 6.05 - index.assets js - Features: Changing under.construction jpg to a new asset

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,11 @@
 import logo from './logo-letters.svg'
-import construction from './under-construction.jpg'
+import construction from './under.construction.jpg'
 
-/**Portfolio-erick - version 6.04 - index assets js -
+/**Portfolio-erick - version 6.05 - index assets js -
  * Features:
  * 
- *      --> Importing and exporting construction
- *          background.
- * 
- *      --> Changing versioning to two digits.
+ *      --> Changing under.construction.jpg for 
+ *          a new asset.
  * 
  * Notes: By this version changes to the data
  * has been made in order to get the essential
