0ae7e833402aff8701dbef0730519dce53112583
Erick Rodriguez <erickrod54@gmail.com>
2023-04-11 13:02:46 -0400

    Portfolio-erick - version 6.06 - App js file - Features: Refactoring layout, Pending to set up again routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React from "react";
 import HomePage from "./pages/home.page";
-import ResumePage from "./pages/resume.page";
-import DashBoard from "./pages/dashboard.page";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-
-/**Portfolio-erick - version 1 - App  js file -
+/**Portfolio-erick - version 6.06 - App  js file -
  * Features:
  * 
- *      --> Building the routing. 
+ *      --> Refactoring layout.
  * 
- *      --> Importing and placing 'HomePage', 
- *          'Dashboard' and 'ResumePage' 
- *          Components 
+ *      --> Pending to set up again routing
  * 
- * Notes: By this version has been implemented
- * the routing using react-router v6
+ * Notes: This file is gonna be changed later.
  */
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/dashboard' element={<DashBoard />}/>
-          <Route path='/resume' element={<ResumePage />}/>
-        </Routes>
-      </Router>
+    <HomePage />
   );
 }
 
