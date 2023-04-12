969ca9f5d929cb70f7098b71e78e6361fdecf3fc
Erick Rodriguez <erickrod54@gmail.com>
2023-04-12 02:09:15 -0400

    Portfolio-erick - version 6.06 - Intro - Features: Refactoring Intro Component, Applying naming convention

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -1,79 +1,42 @@
 import React from "react";
-import styled from "styled-components";
+import { usePortfolioContext } from "../context";
 
-import { useAppContext } from '../context'
-
-/**Portfolio-erick - version 6 - Intro
- * Component - Features:
+/**Portfolio-erick - version 6.06 - Intro - Features:
  * 
- *      --> Finish responsiveness in intro
- *          for small screens
+ *      --> Refactoring Intro Component.
  * 
- * Notes: By this version changes to the data
- * has been made in order to get the essential
- * information
+ *      --> Applying naming convention.
+ * 
+ * Notes: This components is adapted to the new layout
+ * and is functional working, pending for layout the 
+ * component itself
  */
 
-const Intro = () => {
+const IntroComponent = () => {
 
-    const { ResumeData } = useAppContext()
+    const { filePdf } = usePortfolioContext();
 
-    //console.log(ResumeData)
-    
+    const { ResumeData } = usePortfolioContext()
+    console.log('resume data ==>', ResumeData)
     return(
-        <IntroWrapper>
-            {ResumeData.filter((section) => section.id < 2).map((half) => {
-            const { id, myname, intro } = half;
- 
-    //console.log('half ==>', half)
- 
-    return(
-     <div key={id}>
-        <h2 className="title name">Hello i'm {myname}</h2>
-        <div className="text">
-        <p>{intro}</p>
-        </div>
-     </div>
-     )
-    })}
-        </IntroWrapper>
+        <section id='intro'>
+              {ResumeData.filter((section) => section.id < 2).map((half) => {
+                const { id, myname, intro } = half;
+                    //console.log('half ==>', half)
+                    return(
+                    <div key={id} className="intro--intromessage">
+                        <h2 className="intro--intromessage--title">Hello i'm {myname}</h2>
+                        <div className="intro--intromessage--text">
+                        <p>{intro}</p>
+                        </div>
+                        <div className="intro--cv">
+                            <a href='https://drive.google.com/file/d/1-klR8-5g9xBiqcagM2Pn3hA8B1IQzTfX/view?usp=share_link' target="_blank" rel="noreferrer">{filePdf}</a>
+                        </div>
+                    </div>
+                    )
+                 })}
+        </section>
     )
 }
 
-const IntroWrapper = styled.div`
-    display: flex;      
-    width: 50%;
-    justify-content: center;
-    align-items: center;
-
-    .name{
-        color: yellow;
-    }
-
-    
-    
-    @media screen and (max-width: 800px) {
-        padding: 2rem;
-        width: 15rem;
-        width: 50%;
-        h2{
-            font-size: 1rem;
-            width: 120%;
-        }
-        p{
-            font-size:.75rem;  
-        }
-    }
-    
-    @media screen and (max-width: 400px) {
-        width: 50%;
-        h2{
-            font-size: .75rem;
-        }
-        p{
-            font-size:.65rem;  
-        }
-    }
-    `
-
-export default Intro;
+export default IntroComponent;
