b1280ae1e4be605316c25177e48993d9b2fa232d
Erick Rodriguez <erickrod54@gmail.com>
2023-04-13 11:22:47 -0400

    Portfolio-erick - version 6.06 - index.assets js - Features: Refactoring index assets

---
+++ src/assets/index.assets.js
@@ -0,0 +1,14 @@
+import logo from './logo-letters.svg'
+import construction from './under.construction.jpg';
+
+/**Portfolio-erick - version 6.06 - index assets js -
+ * Features:
+ * 
+ *      --> Refactoring index assets.
+ * 
+ * Notes: By this version changes to the data
+ * has been made in order to get the essential
+ * information and email feature
+ */
+
+export { logo, construction }
