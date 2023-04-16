86c1fa96dcb66cec8b9571284bbd8446b5cb6140
Erick Rodriguez <erickrod54@gmail.com>
2023-04-15 20:42:26 -0400

    Portfolio-erick - version 6.06 - index > components - Features: Restructuring components in the index

---
+++ src/components/index.components.js
@@ -0,0 +1,17 @@
+import HeaderComponent from "./header.component";
+import IntroComponent from "./intro.component";
+import SkillsComponent from "./skills.component";
+import Footer from "./footer.component";
+import HeaderButtons from "./header.buttons.component";
+import Contact from "./contact.component";
+
+/**Portfolio-erick - version 6.06 - index > components
+ *  - Features:
+ * 
+ *      --> Restructuring components in the index.
+ * 
+ * Notes: By this version all tasks are done 
+ * for this component
+ */
+
+export { HeaderComponent, IntroComponent, SkillsComponent, HeaderButtons, Footer, Contact }
