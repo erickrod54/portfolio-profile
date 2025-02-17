bc63bfbd0700b531b0dbbefd667a34e7874f7db3
Erick Rodriguez <erickrod54@gmail.com>
2025-02-16 22:03:04 -0500

    Portfolio-erick - version 6.07 - Footer - Features: Refactoring to Styled Components

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { usePortfolioContext } from "../context";
+import styled from "styled-components";
 
-/**Portfolio-erick - version 6.06 - Footer - Features:
+
+/**Portfolio-erick - version 6.07 - Footer - Features:
  * 
- *      --> Refactoring footer.
- * 
- *      --> Applying naming convention.
+ *      --> Refactoring to Styled Components
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
+const FooterContainer = styled.section`
+  display: grid;
+  margin: 2rem;
+  padding: 2rem;
+  grid-template-columns: repeat(2, 1fr); /* divide container into 3 equal columns */
+  grid-column: container-start / container-end;
+  background: linear-gradient(
+    to left,
+    rgb(48, 7, 82) 0%,
+    rgba(0, 128, 128, 1) 100%
+  );
+  border-radius: 2rem;
+  margin-left: 4.3rem;
+  margin-right: 4.3rem;
+`
+
+
 const Footer = () => {
 
     const { social } = usePortfolioContext()
 
     return(
-        <section id='footer'>
+        <FooterContainer>
             <div className="footer--center">
                 <ul>
                     <li>
@@ -54,7 +71,7 @@ const Footer = () => {
                     )
                 })}
             </div>
-        </section>
+        </FooterContainer>
     )
 }
 
