6abfe703e9c96ecbf8a37fbe3a63ad7857a9bd36
Erick Rodriguez <erickrod54@gmail.com>
2025-02-17 21:30:00 -0500

    Portfolio-erick - version 6.13 - Footer - Features: Migrating FooterContainer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,32 +1,16 @@
 import React from "react";
 import { usePortfolioContext } from "../context";
 import styled from "styled-components";
+import { FooterContainer } from "../styled-components/styled.components";
 
-
-/**Portfolio-erick - version 6.12 - Footer - Features:
+/**Portfolio-erick - version 6.13 - Footer - Features:
  * 
- *      --> writting 'AnchorTag' 
+ *      --> Migrating 'FooterContainer' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
-
-const FooterContainer = styled.section`
-  display: grid;
-  margin: 2rem;
-  padding: 2rem;
-  grid-template-columns: repeat(2, 1fr); /* divide container into 3 equal columns */
-  grid-column: container-start / container-end;
-  background: linear-gradient(
-    to left,
-    rgb(48, 7, 82) 0%,
-    rgba(0, 128, 128, 1) 100%
-  );
-  border-radius: 2rem;
-  margin-left: 4.3rem;
-  margin-right: 4.3rem;
-`
 
 const FooterCenter = styled.div`
   justify-self: end;
