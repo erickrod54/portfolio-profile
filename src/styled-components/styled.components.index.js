b22546c1a168e17912fc4a4598aff9e0bdbace62
Erick Rodriguez <erickrod54@gmail.com>
2025-02-17 21:31:00 -0500

    Portfolio-erick - version 6.13 - styled.componennts - Features: Importing and exporting FooterContainer

---
+++ src/styled-components/styled.components.index.js
@@ -0,0 +1,14 @@
+import { FooterContainer } from "./styled.components";
+
+
+/**Portfolio-erick - version 6.13 - styled.components - Features:
+ * 
+ *      --> importing and exporting  'FooterContainer' 
+ * 
+ * Notes: This components is adapted to the new layout
+ * and is functional working, pending for layout the 
+ * component itself
+ */
+
+export { FooterContainer }
+
