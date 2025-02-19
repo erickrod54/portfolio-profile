ff34fe75c1b3f1fe976a51f56b4111ee5a656a1d
Erick Rodriguez <erickrod54@gmail.com>
2025-02-18 21:31:00 -0500

    Portfolio-erick - version 6.16 - styled.components - Features: importing and exporting CenterList

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,14 +1,14 @@
-import { FooterCenter, FooterContainer } from "./styled.components";
+import { CenterList, FooterCenter, FooterContainer } from "./styled.components";
 
 
-/**Portfolio-erick - version 6.13 - styled.components - Features:
+/**Portfolio-erick - version 6.16 - styled.components - Features:
  * 
- *      --> importing and exporting  'FooterContainer' 
+ *      --> importing and exporting  'CenterList' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-export { FooterContainer, FooterCenter }
+export { FooterContainer, FooterCenter, CenterList }
 
