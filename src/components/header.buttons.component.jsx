72767d384c51a4332c7b3a0a5d0387e606a388af
Erick Rodriguez <erickrod54@gmail.com>
2025-02-20 21:31:00 -0500

    Portfolio-erick - version 7.00 - HeaderButtons - Features: MigratingEachBtn

--- src/components/header.buttons.component.jsx
+++ src/components/header.buttons.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { styled } from "styled-components";
+import { EachBtn } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 6.08 - HeaderButtons - Features:
+/**Portfolio-erick - version 7.00 - HeaderButtons - Features:
  * 
- *      --> Refactoring styles to add box shadow efffect.
+ *      --> Migrating 'EachBt'.
  * 
  * Notes: By this version all tasks are done 
  * for this component
@@ -25,33 +26,5 @@ const HeaderButtons = ({ id, urlname, openModal }) => {
     )
 }
 
-const EachBtn = styled.button`
-  padding: 10px 20px;
-  text-transform: uppercase;
-  border-radius: 8px;
-  font-size: 17px;
-  font-weight: 500;
-  color: var(--clr-white);
-  text-shadow: none;
-  background: transparent;
-  box-shadow: transparent;
-  border: 2px solid var(--clr-white);;
-  transition: 0.5s ease;
-  user-select: none;
 
-&:hover,:focus {
-  color: #ffffff;
-  background: #008cff;
-  border: 1px solid #008cff;
-  text-shadow: 0 0 5px #ffffff,
-              0 0 10px #ffffff,
-              0 0 20px #ffffff;
-  box-shadow: 0 0 5px #008cff,
-              0 0 20px #008cff,
-              0 0 50px #008cff,
-              0 0 100px #008cff;
- cursor: pointer;
-}
-
-`
 export default HeaderButtons;
