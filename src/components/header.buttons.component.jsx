8a0cacdb0cd8a4555fb5a922c42fad7f5ccc1f49
Erick Rodriguez <erickrod54@gmail.com>
2025-02-20 21:32:00 -0500

    Portfolio-erick - version 7.01 - HeaderButtons - Features: writting ButtonsWrapper

--- src/components/header.buttons.component.jsx
+++ src/components/header.buttons.component.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import { styled } from "styled-components";
 import { EachBtn } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 7.00 - HeaderButtons - Features:
+/**Portfolio-erick - version 7.01 - HeaderButtons - Features:
  * 
- *      --> Migrating 'EachBt'.
+ *      --> writting 'ButtonsWrapper'.
  * 
  * Notes: By this version all tasks are done 
  * for this component
  */
+
+const ButtonsWrapper = styled.section`
+  display: grid;
+  place-content: center;
+  width: 5vw;
+  height: 5vh;
+  border-radius: 0.7rem;
+  cursor: pointer;
+`
+
 const HeaderButtons = ({ id, urlname, openModal }) => {
 
     console.log('open modal value ==>', openModal)
@@ -16,13 +26,13 @@ const HeaderButtons = ({ id, urlname, openModal }) => {
     /**className={'header--buttons--each' ? 'header--buttons--each' : 'header--buttons--each-small'} */
 
     return(
-        <section id='header--buttons--each'>
+        <ButtonsWrapper>
         <ul key={id}>
                        <EachBtn id="btn" onClick={ urlname === 'contact' ? openModal : null}>
                         {urlname} 
                        </EachBtn>     
         </ul>
-        </section>
+        </ButtonsWrapper>
     )
 }
 
