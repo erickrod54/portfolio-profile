8da4424b881844ee5731c4f312602698510fa80d
Erick Rodriguez <erickrod54@gmail.com>
2025-02-20 21:30:00 -0500

    Portfolio-erick - version 7.00 - styled.components - Features: importing and exporting EachBtn

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,14 +1,13 @@
-import { AnchorTag, CenterList, CenterListItem, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
+import { AnchorTag, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
 
-
-/**Portfolio-erick - version 6.20 - styled.components - Features:
+/**Portfolio-erick - version 7.00 - styled.components - Features:
  * 
- *      --> importing and exporting  'AnchorTag' 
+ *      --> importing and exporting  'EachBtn' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag }
+export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn }
 
