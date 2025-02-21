d379b4a322ab5fbe254c5547e457a4861c7388c1
Erick Rodriguez <erickrod54@gmail.com>
2025-02-20 21:34:00 -0500

    Portfolio-erick - version 7.00 - styled.components - Features: importing and exporting ButtonsWrapper

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,13 +1,13 @@
-import { AnchorTag, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
+import { AnchorTag, ButtonsWrapper, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
 
 /**Portfolio-erick - version 7.00 - styled.components - Features:
  * 
- *      --> importing and exporting  'EachBtn' 
+ *      --> importing and exporting  'ButtonsWrapper' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn }
+export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper }
 
