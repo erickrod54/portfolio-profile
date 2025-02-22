40f45d6deaaf56ab34686d9b9e67e975529dffd7
Erick Rodriguez <erickrod54@gmail.com>
2025-02-22 16:50:14 -0500

    Portfolio-erick - version 7.08 - HeaderComponent - Features: writting HeaderWrapper

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -2,23 +2,32 @@ import React from "react";
 import { logo } from "../assets/index.assets";
 import { usePortfolioContext } from "../context";
 import HeaderButtons from "./header.buttons.component";
+import styled from "styled-components";
 
-/**Portfolio-erick - version 6.06 - HeaderComponent - Features:
+/**Portfolio-erick - version 7.08 - HeaderComponent - Features:
  * 
- *      --> Refactoring NavBar to HeaderComponent
- * 
- *      --> Adapting HeaderComponent to the new layout.
+ *      --> writting 'HeaderWrapper'
  * 
  * Notes: By this version all tasks are done 
  * for this component
  */
+
+const HeaderWrapper = styled.section`
+   display: grid;
+    margin: 2rem;
+    padding: 2rem;
+    grid-column: container-start / container-end;
+    height: fit-content;
+    background: linear-gradient(to left, rgb(48, 7, 82) 0%, rgba(0, 128, 128, 1) 100%);
+    border-radius: 4rem;
+`
 
 const HeaderComponent = () => {
 
     const { openModal, NavBarData } = usePortfolioContext()
 
     return(
-        <section id='header'>
+        <HeaderWrapper>
                 <div className="header--logo">
                     <img src={logo} alt='logo'/>
                 </div>
@@ -31,7 +40,7 @@ const HeaderComponent = () => {
                     )
                 })} 
                 </div>
-        </section>
+        </HeaderWrapper>
     )
 }
 
