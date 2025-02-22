3ac579fc9eb8d95f0ccc6166413c2a117a3b7f8c
Erick Rodriguez <erickrod54@gmail.com>
2025-02-22 16:54:45 -0500

    Portfolio-erick - version 7.09 - styled.components - Features: Adding HeaderWrapper

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,13 +1,13 @@
-import { AnchorTag, ButtonsList, ButtonsWrapper, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
+import { AnchorTag, ButtonsList, ButtonsWrapper, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight, HeaderWrapper } from "./styled.components";
 
-/**Portfolio-erick - version 7.06- styled.components - Features:
+/**Portfolio-erick - version 7.09- styled.components - Features:
  * 
- *      --> Adding  'CenterListItem' 
+ *      --> Adding  'HeaderWrapper' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper, ButtonsList, CenterListItem }
+export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper, ButtonsList, CenterListItem, HeaderWrapper }
 
