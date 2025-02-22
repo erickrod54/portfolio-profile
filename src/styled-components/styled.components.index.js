6294efaca888d22fd328a80e713d32460c564f8d
Erick Rodriguez <erickrod54@gmail.com>
2025-02-21 21:34:00 -0500

    Portfolio-erick - version 7.05 - styled.components - Features: Adding ButtonsList

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,13 +1,13 @@
 import { AnchorTag, ButtonsList, ButtonsWrapper, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
 
-/**Portfolio-erick - version 7.04- styled.components - Features:
+/**Portfolio-erick - version 7.05- styled.components - Features:
  * 
- *      --> importing and exporting  'ButtonsList' 
+ *      --> Adding  'ButtonsList' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper }
+export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper, ButtonsList }
 
