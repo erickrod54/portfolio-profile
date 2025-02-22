ed97c9983adab52ad208b9d150ee5cd3a0cbebe3
Erick Rodriguez <erickrod54@gmail.com>
2025-02-21 21:35:00 -0500

    Portfolio-erick - version 7.06 - styled.components - Features: Adding CenterListItem

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,13 +1,13 @@
 import { AnchorTag, ButtonsList, ButtonsWrapper, CenterList, CenterListItem, EachBtn, FooterCenter, FooterContainer, FooterRight } from "./styled.components";
 
-/**Portfolio-erick - version 7.05- styled.components - Features:
+/**Portfolio-erick - version 7.06- styled.components - Features:
  * 
- *      --> Adding  'ButtonsList' 
+ *      --> Adding  'CenterListItem' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper, ButtonsList }
+export { FooterContainer, FooterCenter, CenterList, FooterRight, AnchorTag, EachBtn, ButtonsWrapper, ButtonsList, CenterListItem }
 
