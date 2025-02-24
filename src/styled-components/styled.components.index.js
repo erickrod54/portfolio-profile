00eb7649e07a7878cfb62369ae9e25664d034d40
Erick Rodriguez <erickrod54@gmail.com>
2025-02-24 13:33:19 -0500

    Portfolio-erick - version 7.15 - styled.components - Features: Adding IntroSection

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -5,11 +5,12 @@ import { AnchorTag,
          CenterListItem,
          EachBtn,
          FooterCenter,
-         FooterContainer, FooterRight, HeaderButtonsWrapper, HeaderLogo, HeaderWrapper } from "./styled.components";
+         FooterContainer, FooterRight, HeaderButtonsWrapper, HeaderLogo, HeaderWrapper, 
+         IntroSection} from "./styled.components";
 
-/**Portfolio-erick - version 7.14 - styled.components - Features:
+/**Portfolio-erick - version 7.15 - styled.components - Features:
  * 
- *      --> Adding  'HeaderButtonsWrapper' 
+ *      --> Adding  'IntroSection' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
