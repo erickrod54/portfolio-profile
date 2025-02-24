3296c2a073436f78a6c4556fc7f6a84713316f50
Erick Rodriguez <erickrod54@gmail.com>
2025-02-23 21:31:00 -0500

    Portfolio-erick - version 7.11 - HeaderComponent - Features: Migrating HeaderLogo

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -3,19 +3,16 @@ import { logo } from "../assets/index.assets";
 import { usePortfolioContext } from "../context";
 import HeaderButtons from "./header.buttons.component";
 import styled from "styled-components";
-import { HeaderWrapper } from "../styled-components/styled.components";
+import { HeaderLogo, HeaderWrapper } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 7.10 - HeaderComponent - Features:
+/**Portfolio-erick - version 7.11 - HeaderComponent - Features:
  * 
- *      --> writting  'HeaderLogo'
+ *      --> Migrating  'HeaderLogo'
  * 
  * Notes: By this version all tasks are done 
  * for this component
  */
 
-const HeaderLogo = styled.div`
-  grid-column: col-start 0 / col-end 1;
-`;
 
 const HeaderComponent = () => {
 
