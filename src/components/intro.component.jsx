53d060100d0c7ddfe06d28837073f6b20eb9deb5
Erick Rodriguez <erickrod54@gmail.com>
2025-02-26 21:34:00 -0500

    Portfolio-erick - version 7.20 - Intro - Features: writting IntroText

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -3,15 +3,19 @@ import { usePortfolioContext } from "../context";
 import styled from "styled-components";
 import { IntroMessage, IntroSection, IntroTitle } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 7.19 - Intro - Features:
+/**Portfolio-erick - version 7.20 - Intro - Features:
  * 
- *      --> Migrating 'IntroTitle'.
+ *      --> writting 'IntroText'.
  * 
 *     
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
+
+const IntroText = styled.p`
+  color: var(--clr-white);
+`;
 
 const IntroComponent = () => {
 
@@ -27,9 +31,9 @@ const IntroComponent = () => {
                     return(
                     <IntroMessage key={id}>
                         <IntroTitle className="intro--intromessage--title">Hello i'm {myname}</IntroTitle>
-                        <div className="intro--intromessage--text">
-                        <p>{intro}</p>
-                        </div>
+                        <IntroText>
+                            {intro}
+                        </IntroText>
                         <div className="intro--cv">
                             <a href='https://drive.google.com/file/d/1-klR8-5g9xBiqcagM2Pn3hA8B1IQzTfX/view?usp=share_link' target="_blank" rel="noreferrer">{filePdf}</a>
                         </div>
