3ed9f45496573aac83ab45931c10ec17dcd1c1cf
Erick Rodriguez <erickrod54@gmail.com>
2025-02-27 21:35:00 -0500

    Portfolio-erick - version 8.02 - Intro - Features: Migrating DownloadCV

--- src/components/intro.component.jsx
+++ src/components/intro.component.jsx
@@ -1,41 +1,17 @@
 import React from "react";
 import { usePortfolioContext } from "../context";
 import styled from "styled-components";
-import { IntroMessage, IntroSection, IntroText, IntroTitle } from "../styled-components/styled.components";
+import { DownloadCV, IntroMessage, IntroSection, IntroText, IntroTitle } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 8.01 - Intro - Features:
+/**Portfolio-erick - version 8.02 - Intro - Features:
  * 
- *      --> writting 'DownloadCV'.
+ *      --> Migrating 'DownloadCV'.
  * 
 *     
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
-
-const DownloadCV = styled.div`
-  margin-top: 3rem;
-  padding: 10px 20px;
-  text-transform: uppercase;
-  border-radius: 8px;
-  font-size: 17px;
-  font-weight: 500;
-  color: var(--clr-white);
-  text-shadow: none;
-  background: transparent;
-  box-shadow: transparent;
-  transition: 0.5s ease;
-  user-select: none;
-
-  &:hover, &:focus {
-    color: #ffffff;
-    background: #008cff;
-    border: 1px solid #008cff;
-    text-shadow: 0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 20px #ffffff;
-    box-shadow: 0 0 5px #008cff, 0 0 20px #008cff, 0 0 50px #008cff, 0 0 100px #008cff;
-    cursor: pointer;
-  }
-`;
 
 const IntroComponent = () => {
 
