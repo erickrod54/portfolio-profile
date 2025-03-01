5ece1b7aa577dc2cee7f0aa679f818c0924e32a4
Erick Rodriguez <erickrod54@gmail.com>
2025-02-28 21:30:00 -0500

    Portfolio-erick - version 8.03 - SkillsComponent - Features: writting SkillsSection

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { usePortfolioContext } from "../context";
+import styled from "styled-components";
 
-/**Portfolio-erick - version 6.06 - SkillsComponent
+/**Portfolio-erick - version 8.03 - SkillsComponent
  *  - Features:
  * 
- *      --> Refactoring SkillsComponent Component.
- * 
- *      --> Applying style naming convention.
+ *      --> writting 'SkillsSection'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
+const SkillsSection = styled.section`
+  display: grid;
+  grid-column: container-start / container-end;
+  place-content: center;
+  background: var(--gradient-bkgd);
+  margin: 4rem;
+  border-radius: 4rem;
+`;
+
 const SkillsComponent = () => {
 
     const { ResumeData } = usePortfolioContext()
 
     return(
-        <section id='skills'>
+        <SkillsSection>
             <div className="skills--box">
             <h2 className="skills--title">my skills and growing:</h2>
             {ResumeData.filter((section) => section.id === 2).map((skillssection) =>{
@@ -37,7 +45,7 @@ const SkillsComponent = () => {
                 )
              })}
             </div>
-        </section>
+        </SkillsSection>
     )
 }
 
