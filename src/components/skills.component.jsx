d319024babb34f57a03d42fb75931ae4b7cf2125
Erick Rodriguez <erickrod54@gmail.com>
2025-03-01 21:31:00 -0500

    Portfolio-erick - version 8.06 - SkillsComponent - Features: Migrating SkillsSection

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -1,27 +1,19 @@
 import React from "react";
 import { usePortfolioContext } from "../context";
 import styled from "styled-components";
-import { SkillsSection } from "../styled-components/styled.components";
+import { SkillsBox, SkillsSection } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 8.05 - SkillsComponent
+/**Portfolio-erick - version 8.06 - SkillsComponent
  *  - Features:
  * 
- *      --> writting 'SkillsSection'
+ *      --> Migrating 'SkillsSection'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
  * component itself
  */
 
-const SkillsBox = styled.div`
-  display: grid;
-  border: 1px solid white;
-  margin: 1.7em;
-  border-radius: 0.75em;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-`;
+
 
 const SkillsComponent = () => {
 
