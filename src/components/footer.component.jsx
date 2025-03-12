1bd57f5f9746b7c1b8b0044edd8d44b362306671
Erick Rodriguez <erickrod54@gmail.com>
2025-03-12 21:31:00 +0000

    Portfolio-erick - version 9.14 - Footer - Features: Clearing imports

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,11 +1,9 @@
-import React from "react";
 import { usePortfolioContext } from "../context";
-import styled from "styled-components";
 import { AnchorTag, CenterList, CenterListItem, FooterCenter, FooterContainer, FooterRight } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 6.20 - Footer - Features:
+/**Portfolio-erick - version 9.14 - Footer - Features:
  * 
- *      --> Migrating 'AnchorTag' 
+ *      --> Clearing Imports
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
