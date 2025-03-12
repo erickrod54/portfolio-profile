4eed1054da358cb623050a2681244f048dc4ff7e
Erick Rodriguez <erickrod54@gmail.com>
2025-03-12 21:33:00 +0000

    Portfolio-erick - version 9.16 - HeaderComponent - Features: Clearing imports

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,13 +1,11 @@
-import React from "react";
 import { logo } from "../assets/index.assets";
 import { usePortfolioContext } from "../context";
 import HeaderButtons from "./header.buttons.component";
-import styled from "styled-components";
 import { HeaderButtonsWrapper, HeaderLogo, HeaderWrapper } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 7.14 - HeaderComponent - Features:
+/**Portfolio-erick - version 9.16 - HeaderComponent - Features:
  * 
- *      --> Migrating  'HeaderButtonsWrapper'
+ *      --> Clearing Imports
  * 
  * Notes: By this version all tasks are done 
  * for this component
