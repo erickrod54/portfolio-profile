31f116db9ad08c814894f5d0979c26e5dfc5b3f8
Erick Rodriguez <erickrod54@gmail.com>
2025-03-14 21:32:00 +0000

    Portfolio-erick - version 10.03 - index assets js - Features: Clearing construction asset

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,16 +1,15 @@
 import logo from './logo-letters.svg'
-import construction from './under.construction.jpg';
 import github from './icons/icons8-github.svg';
 import linkedin from './icons/icons8-linkedin.svg';
 
-/**Portfolio-erick - version 6.07 - index assets js -
+/**Portfolio-erick - version 10.03 - index assets js -
  * Features:
  * 
- *      --> Importing and providing 'github' and 'linkedin'.
+ *      --> Clearing 'construction' asset.
  * 
  * Notes: By this version changes to the data
  * has been made in order to get the essential
  * information and email feature
  */
 
-export { logo, construction, github, linkedin }
+export { logo, github, linkedin }
