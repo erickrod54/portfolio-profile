d9f8128b27a609741f562a8107b8f65fec8ec530
Erick Rodriguez <erickrod54@gmail.com>
2025-03-15 21:35:00 -0500

    Portfolio-erick - version 10.06 - index > components - Features: Adding Card Component

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -4,14 +4,20 @@ import SkillsComponent from "./skills.component";
 import Footer from "./footer.component";
 import HeaderButtons from "./header.buttons.component";
 import Contact from "./contact.component";
+import Card from "./card.component";
 
-/**Portfolio-erick - version 6.06 - index > components
+/**Portfolio-erick - version 10.06 - index > components
  *  - Features:
  * 
- *      --> Restructuring components in the index.
+ *      --> Adding Card Component.
  * 
  * Notes: By this version all tasks are done 
  * for this component
  */
 
-export { HeaderComponent, IntroComponent, SkillsComponent, HeaderButtons, Footer, Contact }
+export { HeaderComponent,
+         IntroComponent,
+         SkillsComponent,
+         HeaderButtons,
+         Footer,
+         Contact, Card }
