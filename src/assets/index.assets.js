8c2e51fa1f7bd6c2645e34d95d3bcc8f0dd13160
Erick Rodriguez <erickrod54@gmail.com>
2025-03-17 21:34:00 -0500

    Portfolio-erick - version 10.08 - index assets js - Features: Exporting profileImage asset

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,15 +1,16 @@
 import logo from './logo-letters.svg'
 import github from './icons/icons8-github.svg';
 import linkedin from './icons/icons8-linkedin.svg';
+import profileImage from './Profile_picture_2.jpg'
 
-/**Portfolio-erick - version 10.03 - index assets js -
+/**Portfolio-erick - version 10.08 - index assets js -
  * Features:
  * 
- *      --> Clearing 'construction' asset.
+ *      --> Exporting 'profileImage' asset.
  * 
  * Notes: By this version changes to the data
  * has been made in order to get the essential
  * information and email feature
  */
 
-export { logo, github, linkedin }
+export { logo, github, linkedin, profileImage }
