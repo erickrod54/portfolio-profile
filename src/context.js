e17e13870b9fe7e5236e0c11a103f8d229f7bb6a
Erick Rodriguez <erickrod54@gmail.com>
2025-03-18 21:31:00 -0500

    Portfolio-erick - version 10.09 - context js - Features: Importing and providing profileImagePic

--- src/context.js
+++ src/context.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from "react";
-import { filePdf, NavBarData, ResumeData, skillIcons, social } from "./data";
+import { filePdf, NavBarData, profileImagePic, ResumeData, skillIcons, social } from "./data";
 
-/**Portfolio-erick - version 9.13 - context js
+/**Portfolio-erick - version 10.09 - context js
  *  - Features:
  *  
- *      --> Importing and providing 'skillIcons'.
+ *      --> Importing and providing 'profileImagePic'.
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -36,6 +36,7 @@ export const PortfolioProvider = ({ children }) => {
                 closeModal,
                 filePdf,
                 skillIcons,
+                profileImagePic
                 }}>
             {children}
         </PortfolioContext.Provider>
