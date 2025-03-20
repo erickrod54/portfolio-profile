4dbbdd10a8d981b069d63303bcdbb6dc1458406b
Erick Rodriguez <erickrod54@gmail.com>
2025-03-19 21:34:00 -0500

    Portfolio-erick - version 10.13 - SocialComponent - Features: Building SocialComponent

---
+++ src/components/social.component.jsx
@@ -0,0 +1,118 @@
+import { faTelegram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+/**Portfolio-erick - version 10.13 - SocialComponent -
+* Features:
+
+    --> Building 'SocialComponent'
+
+* Notes: This is the 'SocialComponent' isolation
+* so the styles and behavior can be better handled
+**/
+
+const StyleSocialWrapper = styled.div`
+ .content {
+    position: absolute;
+    z-index: 3;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    transform: scale(0);
+    transition: 0.5s;
+  }
+
+  .content .title {
+    position: relative;
+    color: #fff;
+    font-weight: 500;
+    line-height: 1em;
+    font-size: 1em;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  .content .title span {
+    font-weight: 300;
+    font-size: 0.70em;
+  }
+
+  .content .sci {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    margin-top: 5px;
+  }
+
+  .sci li {
+    list-style: none;
+  }
+
+  .sci li .fa-brands {
+    width: 14px;
+  }
+
+  .sci li .fa-facebook {
+    width: 10px;
+  }
+
+  .sci li a {
+    position: relative;
+    text-decoration: none;
+    color: rgba(255, 255, 255, 0.5);
+    background: rgba(0, 0, 0, 0.2);
+    fill: currentColor;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    transition: 0.5s;
+    margin-right:2rem;
+  }
+
+  .sci li a:hover {
+    fill: currentColor;
+    color: rgba(255, 255, 255, 1);
+  }
+`
+
+const SocialComponent = () => {
+
+    return(
+        <StyleSocialWrapper>    
+        <div className="content">
+          <p className="title">Erick Rodriguez<br /><span>Software Engineer</span></p>
+          <ul className="sci">
+            <li>
+              <a href="/#">
+                <FontAwesomeIcon icon={faTelegram}/>
+              </a>
+            </li>
+            <li>
+              <a href="/#">
+                <FontAwesomeIcon icon={faXTwitter}/>
+              </a>
+            </li>
+            <li>
+              <a href="/#">
+                <svg className="fa-brands fa-linkedin-in" width={24} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" /></svg>
+              </a>
+            </li>
+            <li>
+              <a href="/#">
+                <svg className="fa-brands fa-instagram" width={24} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" /></svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+        </StyleSocialWrapper>
+    )
+}
+
+export default SocialComponent;
