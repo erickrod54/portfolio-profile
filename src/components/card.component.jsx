d11fb5df7dee86249a93ccab765c94b02beb3ab5
Erick Rodriguez <erickrod54@gmail.com>
2025-03-20 21:35:00 -0500

    Portfolio-erick - version 10.16 - Card component - Features: Building CardWrapper style component to clean the previous classic .card class

--- src/components/card.component.jsx
+++ src/components/card.component.jsx
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 import ProfileCard from './profile.card.component';
 import { SocialComponent } from './index.components';
 
-/**Portfolio-erick - version 10.13 - Card component -
+/**Portfolio-erick - version 10.16 - Card component -
 * Features:
 
-    -→> Importing and Placing SocialComponent
+    -→> Building 'CardWrapper' style component
+        to clean the previous classic .card class
 
 * Notes: side effects on AvatarPicture comes from:
 *
@@ -27,14 +28,14 @@ import { SocialComponent } from './index.components';
 const Card = () => {
   return (
     <StyledWrapper>
-      <div className="card">
+      <CardWrapper>
         <StyleProfileWrapper>
           <ProfileCard />      
         </StyleProfileWrapper>
         <b />
         <SocialComponent />
-      </div>
-      
+      </CardWrapper>
+
     </StyledWrapper>
     
   );
@@ -43,6 +44,76 @@ const Card = () => {
 const StyleProfileWrapper = styled.div`
 position: relative;
 z-index:4
+`
+
+const CardWrapper = styled.div`
+    position: relative;
+    width: 190px;
+    height: 254px;
+    background: #f00;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 18rem;
+    padding: 18rem;
+
+    &::before{
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(315deg,#03a9f4,#ff0058);    
+    }
+  
+    &::after{
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(315deg,#03a9f4,#ff0058);
+      filter: blur(30px);
+    }
+
+    b {
+      position:absolute;
+      inset:6px;
+      background:rgba(0, 0, 0 ,0.6);
+      z-index: 2;
+    }
+    
+    img{
+      position: absolute;
+      z-index:3;
+      scale:0.8;
+      /*opacity:0.25;**/
+      transition:0.5s;
+    }
+    
+    &:hover img {
+      scale:0.5;
+      opacity:0.9;
+      /*transform: translateY(-70px);**/
+    }
+    
+    .content{
+      position: absolute;
+      z-index:3;
+      bottom:0;
+      display:flex;
+      flex-direction:column;
+      align-items:center;
+      transform:scale(0);
+      transition:0.5s;
+    }
+    
+    &:hover .content{
+      transform:scale(1);
+      bottom:25px;
+    }
 `
 
 const StyledWrapper = styled.div`
@@ -54,76 +125,7 @@ div{
 
 }
 
-  .card {
-    position: relative;
-    width: 190px;
-    height: 254px;
-    background: #f00;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 18rem;
-    padding: 18rem;
-  }
-
-  .card::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(315deg,#03a9f4,#ff0058);
-  }
-
-  .card::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(315deg,#03a9f4,#ff0058);
-    filter: blur(30px);
-  }
-
-  .card b {
-    position: absolute;
-    inset: 6px;
-    background: rgba(0, 0, 0, 0.6);
-    z-index: 2;
-  }
-
-  .card img {     
-    position: absolute;
-    z-index: 3;
-    scale: 0.8;
-    /*opacity:0.25;**/
-    transition: 0.5s;
-  }
-
-  .card:hover img {
-    scale: 0.5;
-    opacity: 0.9;
-    /*transform: translateY(-70px);**/
-  }
-
-  .card .content {
-    position: absolute;
-    z-index: 3;
-    bottom: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    transform: scale(0);
-    transition: 0.5s;
-  }
-
-  .card:hover .content {
-    transform: scale(1);
-    bottom: 25px;
-  }
-
+  
   `;
 
 export default Card;
