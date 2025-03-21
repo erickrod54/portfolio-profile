806fdd4612e26db1d61c3ef2c9c79e2a9f147423
Erick Rodriguez <erickrod54@gmail.com>
2025-03-20 21:31:00 -0500

    Portfolio-erick - version 10.14 - SocialComponent - Features: Replacing svg LinkedinIn to FontAwesome faLinkedinIn

--- src/components/social.component.jsx
+++ src/components/social.component.jsx
@@ -1,11 +1,12 @@
-import { faTelegram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn, faTelegram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
-/**Portfolio-erick - version 10.13 - SocialComponent -
+/**Portfolio-erick - version 10.14 - SocialComponent -
 * Features:
 
-    --> Building 'SocialComponent'
+    --> Replacing svg 'LinkedinIn' to FontAwesome 
+        'faLinkedinIn'
 
 * Notes: This is the 'SocialComponent' isolation
 * so the styles and behavior can be better handled
@@ -101,7 +102,7 @@ const SocialComponent = () => {
             </li>
             <li>
               <a href="/#">
-                <svg className="fa-brands fa-linkedin-in" width={24} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" /></svg>
+                <FontAwesomeIcon icon={faLinkedinIn} />
               </a>
             </li>
             <li>
