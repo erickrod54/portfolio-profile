0985d41b1ec91dc1e607b8d166205bb02efdc758
Erick Rodriguez <erickrod54@gmail.com>
2025-03-22 21:32:00 -0500

    Portfolio-erick - version 10.20 - AvatarPicture - Features: Scoping side effects from CardWrapper > ProfileCard on AvatarPicture

--- src/components/avatar.component.jsx
+++ src/components/avatar.component.jsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 import { usePortfolioContext } from "../context";
 
-/**Portfolio-erick - version 10.15 - AvatarPicture -
+/**Portfolio-erick - version 10.20 - AvatarPicture -
 * Features:
 
-    --> Reducing size on 'ProfilePictureImg'
+    --> Scoping side effects from CardWrapper 
+        > ProfileCard on AvatarPicture
 
-* Notes: This is the Avatar isolation in order to
-* fix bugs related with the overflow, and image 
-* blur - already fixed - reference in Card.component
+* Notes: 
+This way AvatarPicture Component will have full
+control of the side effects cause in previous versions
+for CardWrapper and solve:
+*
+*  -`This is the Avatar isolation in order to
+*  -fix bugs related with the overflow, and image 
+*  -blur - already fixed - reference in Card.component`
+*
+*  the above issue from previus versions
 **/
 
 const ProfileCardCenterButton = styled.button`
@@ -42,6 +50,20 @@ const ProfileCardCenterButton = styled.button`
     transform: none; 
   }
   
+  img{
+      position: absolute;
+      z-index:3;
+      scale:0.8;
+      /*opacity:0.25;**/
+      transition:0.5s;
+    }
+    
+    &:hover img {
+      scale:0.7;
+      opacity:0.9;
+      /*transform: translateY(-70px);**/
+    }
+      
   -webkit-tap-highlight-color: transparent;
 `;
 
