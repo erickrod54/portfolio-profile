1551933bc4bbce561f07b8a9e8ee2c3cdaa7df79
Erick Rodriguez <erickrod54@gmail.com>
2025-03-24 21:31:00 -0500

    Portfolio-erick - version 11.04 - styled.components - Features: Adding ProfileCardCenterButton

--- src/styled-components/styled.components.js
+++ src/styled-components/styled.components.js
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
-/**Portfolio-erick - version 11.03 - styled.components - Features:
+/**Portfolio-erick - version 11.04 - styled.components - Features:
  * 
- *      --> Adding 'ProfilePictureImg' 
+ *      --> Adding 'ProfileCardCenterButton' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -113,6 +113,54 @@ export const ProfilePictureImg = styled.img`
     object-fit: cover; 
     display: block;
   `;
+
+export const ProfileCardCenterButton = styled.button`
+  /* Set the exact size of the final avatar circle */
+  width: 200px; /* w-[200px] */
+  height: 200px; /* h-[200px] */
+  background: radial-gradient(circle at center, #3D3D3D 0%, #000000 100%);
+  
+  /* Make it a perfect circle */
+  border-radius: 9999px; 
+  
+  padding: 0; 
+  border: 2px solid transparent; 
+  
+  /* Center content */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  cursor: pointer;
+  transition: all 500ms;
+  z-index: 0;
+  
+  &:hover {
+    border-color: rgba(156, 163, 175, 0.5); /* hover:border-gray-400/50 */
+    /* Ensure no scaling on hover */
+    transform: none; 
+  }
+  
+  &:active {
+    transform: none; 
+  }
+  
+  img{
+      position: absolute;
+      z-index:3;
+      scale:0.8;
+      /*opacity:0.25;**/
+      transition:0.5s;
+    }
+    
+    &:hover img {
+      scale:0.7;
+      opacity:0.9;
+      /*transform: translateY(-70px);**/
+    }
+      
+  -webkit-tap-highlight-color: transparent;
+`;
 /** Card component styles - END */
 /** Styled Components for new Portfolio - END */
 
