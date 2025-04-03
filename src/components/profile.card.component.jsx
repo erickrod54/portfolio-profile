62eca2de3d0a233120fea831d2543cc44fd4cb5e
Erick Rodriguez <erickrod54@gmail.com>
2025-04-02 21:35:00 -0500

    Portfolio-erick - version 13.05 - ProfileCard - Features: Clearing faSquareJs

--- src/components/profile.card.component.jsx
+++ src/components/profile.card.component.jsx
@@ -1,13 +1,12 @@
 import styled, { css } from 'styled-components';
 import { AvatarPicture, IconComponent } from './index.components';
-import { faSquareJs } from '@fortawesome/free-brands-svg-icons';
 import { usePortfolioContext } from '../context.js';
 import OuterCircleIcons from './outer.circle.icons.component.jsx';
 
-/**Portfolio-erick - version 13.04 - ProfileCard -
+/**Portfolio-erick - version 13.05 - ProfileCard -
 * Features:
 
-    --> Clearing 'faAngular'  
+    --> Clearing 'faSquareJs'  
 
 * Notes: This is the initial test component, and
 * later will be over more refactors.
