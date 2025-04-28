941828017662ef9a188ddd2ab633f451ff6d8abf
Erick Rodriguez <erickrod54@gmail.com>
2025-04-27 21:33:00 -0500

    Portfolio-erick - version 17.02 - styled.components - Features: Clearing StyleModal

--- src/styled-components/styled.components.js
+++ src/styled-components/styled.components.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled, { css } from "styled-components"
 
-/**Portfolio-erick - version 17.00 - styled.components - Features:
+/**Portfolio-erick - version 17.02 - styled.components - Features:
  * 
- *      --> Clearing 'SkillsList'
+ *      --> Clearing 'StyledModal'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -471,21 +471,6 @@ export const ModalContent = styled.div`
   border-radius: var(--radius);
   text-align: center;
   position: relative;
-`;
-
-export const StyledModal = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.75);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  opacity: ${(props) => (props.isOpen ? "1" : "0")};
-  z-index: ${(props) => (props.isOpen ? "999" : "-1")};
-  transition: var(--transition);
 `;
 
 export const DownloadCV = styled.div`
