e2f4b6a5de1ba1ad023e99e674d9b2f92742c628
Erick Rodriguez <erickrod54@gmail.com>
2025-04-27 21:35:00 -0500

    Portfolio-erick - version 17.02 - Modal - Features: Migrating back StyleModal

--- src/legacy_portfolio/components_legacy/modal.component.jsx
+++ src/legacy_portfolio/components_legacy/modal.component.jsx
@@ -1,9 +1,10 @@
+import styled from "styled-components";
 import { usePortfolioContext } from "../context";
 import { ModalContent, StyledModal } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 17.01 - Modal - Features:
+/**Portfolio-erick - version 17.02 - Modal - Features:
  * 
- *      --> Moving Modal component to legacy code
+ *      --> Migrating back 'StyledModal'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -11,6 +12,22 @@ import { ModalContent, StyledModal } from "../styled-components/styled.component
  */
 
 const Modal = ({ children }) => {
+  
+const StyledModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.75);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: ${(props) => (props.isOpen ? "1" : "0")};
+  z-index: ${(props) => (props.isOpen ? "999" : "-1")};
+  transition: var(--transition);
+`;
+
     const { isModalOpen } = usePortfolioContext();
   
     return (
