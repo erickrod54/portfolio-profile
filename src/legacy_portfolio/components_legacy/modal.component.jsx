ee2eedd0b739446cca5417cd89ac88717f5b0ef6
Erick Rodriguez <erickrod54@gmail.com>
2025-04-27 21:31:00 -0500

    Portfolio-erick - version 17.01 - Modal - Features: Moving Modal component to legacy code

---
+++ src/legacy_portfolio/components_legacy/modal.component.jsx
@@ -0,0 +1,25 @@
+import { usePortfolioContext } from "../context";
+import { ModalContent, StyledModal } from "../styled-components/styled.components";
+
+/**Portfolio-erick - version 17.01 - Modal - Features:
+ * 
+ *      --> Moving Modal component to legacy code
+ * 
+ * Notes: This components is adapted to the new layout
+ * and is functional working, pending for layout the 
+ * component itself
+ */
+
+const Modal = ({ children }) => {
+    const { isModalOpen } = usePortfolioContext();
+  
+    return (
+      <StyledModal isOpen={isModalOpen}>
+        <ModalContent>{children}</ModalContent>
+      </StyledModal>
+    );
+  };
+  
+  export default Modal;
+  
+  
