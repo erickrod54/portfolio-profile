03ec3986d098e15817766e8d502116389e39357a
Erick Rodriguez <erickrod54@gmail.com>
2025-05-09 21:31:00 -0500

    Portfolio-erick - version 18.12 - HeaderComponent - Features: Migrating back HeaderWrapper

--- src/legacy_portfolio/components_legacy/header.component.jsx
+++ src/legacy_portfolio/components_legacy/header.component.jsx
@@ -2,10 +2,11 @@ import { logo } from "../assets/index.assets";
 import { usePortfolioContext } from "../context";
 import HeaderButtons from "./header.buttons.component";
 import { HeaderButtonsWrapper, HeaderLogo, HeaderWrapper } from "../styled-components/styled.components";
+import styled from "styled-components";
 
-/**Portfolio-erick - version 18.11 - HeaderComponent - Features:
+/**Portfolio-erick - version 18.12 - HeaderComponent - Features:
  * 
- *      --> Moving 'HeaderComponent' to legacy code
+ *      --> Migrating back 'HeaderWrapper'
  * 
  * Notes: By this version all tasks are done 
  * for this component
@@ -16,6 +17,16 @@ import { HeaderButtonsWrapper, HeaderLogo, HeaderWrapper } from "../styled-compo
 const HeaderComponent = () => {
 
     const { openModal, NavBarData } = usePortfolioContext()
+
+ const HeaderWrapper = styled.section`
+    display: grid;
+    margin: 2rem;
+    padding: 2rem;
+    grid-column: container-start / container-end;
+    height: fit-content;
+    background: linear-gradient(to left, rgb(48, 7, 82) 0%, rgba(0, 128, 128, 1) 100%);
+    border-radius: 4rem;
+`
 
     return(
         <HeaderWrapper>
