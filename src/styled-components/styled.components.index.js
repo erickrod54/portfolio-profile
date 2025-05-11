794efb117ee8c4f71692b2a63367cf885d86eafb
Erick Rodriguez <erickrod54@gmail.com>
2025-05-10 21:32:00 -0500

    Portfolio-erick - version 18.14 - styled.components - Features: Clearing HeaderButtonsWrapper

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -6,7 +6,7 @@ import { AnchorTag,
          EachBtn,
          ExperimentalUIWrapper,
          FooterCenter,
-         FooterContainer, FooterRight, HeaderButtonsWrapper,
+         FooterContainer, FooterRight,
          ModalContent,
          StyleBneonCard,
          StyleProfileWrapper,
@@ -36,9 +36,9 @@ import { AnchorTag,
          SocialContent,
          ProfilePictureImg } from "./styled.components";
 
-/**Portfolio-erick - version 18.13 - styled.components - Features:
+/**Portfolio-erick - version 18.14 - styled.components - Features:
  * 
- *      --> Clearing 'HeaderLogo'
+ *      --> Clearing 'HeaderButtonsWrapper'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -80,5 +80,5 @@ export { FooterContainer,
          StyledSci,
          SocialTitle,
          SocialContent,
-         ButtonsWrapper, ButtonsList, CenterListItem, HeaderButtonsWrapper }
+         ButtonsWrapper, ButtonsList, CenterListItem }
 
