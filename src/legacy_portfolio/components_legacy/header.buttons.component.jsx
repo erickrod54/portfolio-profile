0091e7cc5158b8101cb37e6a8d80754621b3f774
Erick Rodriguez <erickrod54@gmail.com>
2025-05-10 21:35:00 -0500

    Portfolio-erick - version 18.17 - HeaderButtons - Features: Moving HeaderButtons to legacy code

---
+++ src/legacy_portfolio/components_legacy/header.buttons.component.jsx
@@ -0,0 +1,30 @@
+import { ButtonsList, ButtonsWrapper, EachBtn } from "../styled-components/styled.components";
+
+/**Portfolio-erick - version 18.17 - HeaderButtons - Features:
+ * 
+ *      --> Moving 'HeaderButtons' to legacy code.
+ * 
+ * Notes: By this version all tasks are done 
+ * for this component
+ */
+
+
+const HeaderButtons = ({ id, urlname, openModal }) => {
+
+    console.log('open modal value ==>', openModal)
+
+    /**className={'header--buttons--each' ? 'header--buttons--each' : 'header--buttons--each-small'} */
+
+    return(
+        <ButtonsWrapper>
+        <ButtonsList key={id}>
+                       <EachBtn id="btn" onClick={ urlname === 'contact' ? openModal : null}>
+                        {urlname} 
+                       </EachBtn>     
+        </ButtonsList>
+        </ButtonsWrapper>
+    )
+}
+
+
+export default HeaderButtons;
