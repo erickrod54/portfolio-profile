3c6e664325bdf3e02067a8ee3764d277d3dda77f
Erick Rodriguez <erickrod54@gmail.com>
2025-05-11 21:31:00 -0500

    Portfolio-erick - version 18.18 - HeaderButtons - Features: Migrating back ButtonsWrapper

--- src/legacy_portfolio/components_legacy/header.buttons.component.jsx
+++ src/legacy_portfolio/components_legacy/header.buttons.component.jsx
@@ -1,8 +1,9 @@
+import styled from "styled-components";
 import { ButtonsList, ButtonsWrapper, EachBtn } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 18.17 - HeaderButtons - Features:
+/**Portfolio-erick - version 18.18 - HeaderButtons - Features:
  * 
- *      --> Moving 'HeaderButtons' to legacy code.
+ *      --> Migrating back 'ButtonsWrapper'
  * 
  * Notes: By this version all tasks are done 
  * for this component
@@ -14,6 +15,15 @@ const HeaderButtons = ({ id, urlname, openModal }) => {
     console.log('open modal value ==>', openModal)
 
     /**className={'header--buttons--each' ? 'header--buttons--each' : 'header--buttons--each-small'} */
+
+const ButtonsWrapper = styled.section`
+    display: grid;
+    place-content: center;
+    width: 5vw;
+    height: 5vh;
+    border-radius: 0.7rem;
+    cursor: pointer;
+`
 
     return(
         <ButtonsWrapper>
