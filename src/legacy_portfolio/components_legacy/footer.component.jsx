5a6a52fcf9c91bdc19dce1e28e00d9ed65749e1d
Erick Rodriguez <erickrod54@gmail.com>
2025-05-12 21:35:00 -0500

    Portfolio-erick - version 19.04 - Footer - Features: Moving Footer to legacy code

---
+++ src/legacy_portfolio/components_legacy/footer.component.jsx
@@ -0,0 +1,59 @@
+import { usePortfolioContext } from "../context";
+import { AnchorTag, CenterList, CenterListItem, FooterCenter, FooterContainer, FooterRight } from "../styled-components/styled.components";
+
+/**Portfolio-erick - version 19.04 - Footer - Features:
+ * 
+ *      --> Moving 'Footer' to legacy code
+ * 
+ * Notes: This components is adapted to the new layout
+ * and is functional working, pending for layout the 
+ * component itself
+ */
+
+const Footer = () => {
+
+    const { social } = usePortfolioContext()
+
+    return(
+        <FooterContainer>
+            <FooterCenter>
+                <CenterList>
+                    <CenterListItem>
+                        <p>
+                            &copy; { new Date().getFullYear()}        
+                        </p>
+                    </CenterListItem>
+                    <CenterListItem>
+                        <p>
+                            Erick Rodriguez  |  Systems Engineer
+                        </p>
+                    </CenterListItem>
+                    <CenterListItem>
+                        <p>
+                            All Rights reserved
+                        </p>
+                    </CenterListItem>
+                </CenterList>
+            </FooterCenter>
+            <FooterRight>
+                {social.map((net) => {
+                    const { name, url, icon } = net;
+
+                    return(
+                       <AnchorTag key={name} href={url}>
+                       <ul>
+                            <li>
+                                <span>   
+                                {icon}
+                                </span>
+                            </li>
+                       </ul>
+                       </AnchorTag>
+                    )
+                })}
+            </FooterRight>
+        </FooterContainer>
+    )
+}
+
+export default Footer;
