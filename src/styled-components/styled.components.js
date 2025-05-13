e42ee621a32e165c84194942d7a795f98526e243
Erick Rodriguez <erickrod54@gmail.com>
2025-05-12 21:31:00 -0500

    Portfolio-erick - version 19.01 - styled.components - Features: Clearing EachBtn

--- src/styled-components/styled.components.js
+++ src/styled-components/styled.components.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled, { css } from "styled-components"
 
-/**Portfolio-erick - version 18.20 - styled.components - Features:
+/**Portfolio-erick - version 19.01 - styled.components - Features:
  * 
- *      --> Clearing 'ButtonsList'
+ *      --> Clearing 'EachBtn'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -430,36 +430,6 @@ export const ButtonsWrapper = styled.section`
     height: 5vh;
     border-radius: 0.7rem;
     cursor: pointer;
-`
-
-export const EachBtn = styled.button`
-    padding: 10px 20px;
-    text-transform: uppercase;
-    border-radius: 8px;
-    font-size: 17px;
-    font-weight: 500;
-    color: var(--clr-white);
-    text-shadow: none;
-    background: transparent;
-    box-shadow: transparent;
-    border: 2px solid var(--clr-white);;
-    transition: 0.5s ease;
-    user-select: none;
-
-    &:hover,:focus {
-    color: #ffffff;
-    background: #008cff;
-    border: 1px solid #008cff;
-    text-shadow: 0 0 5px #ffffff,
-                0 0 10px #ffffff,
-                0 0 20px #ffffff;
-    box-shadow: 0 0 5px #008cff,
-                0 0 20px #008cff,
-                0 0 50px #008cff,
-                0 0 100px #008cff;
-    cursor: pointer;
-    }
-
 `
 
 export const AnchorTag = styled.a`
