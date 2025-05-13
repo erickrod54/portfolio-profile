b83f90a30b3eec65db57e54b44e0427930c42e3d
Erick Rodriguez <erickrod54@gmail.com>
2025-05-12 21:33:00 -0500

    Portfolio-erick - version 19.01 - HeaderButtons - Features: Migrating back EachBtn

--- src/legacy_portfolio/components_legacy/header.buttons.component.jsx
+++ src/legacy_portfolio/components_legacy/header.buttons.component.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import {  EachBtn } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 19.00 - HeaderButtons - Features:
+/**Portfolio-erick - version 19.01 - HeaderButtons - Features:
  * 
- *      --> Clearing 'ButtonsList' import
+ *      --> Migrating back 'EachBtn' 
  * 
  * Notes: By this version all tasks are done 
  * for this component
@@ -29,6 +29,35 @@ const HeaderButtons = ({ id, urlname, openModal }) => {
 
     /**className={'header--buttons--each' ? 'header--buttons--each' : 'header--buttons--each-small'} */
 
+   const EachBtn = styled.button`
+    padding: 10px 20px;
+    text-transform: uppercase;
+    border-radius: 8px;
+    font-size: 17px;
+    font-weight: 500;
+    color: var(--clr-white);
+    text-shadow: none;
+    background: transparent;
+    box-shadow: transparent;
+    border: 2px solid var(--clr-white);;
+    transition: 0.5s ease;
+    user-select: none;
+
+    &:hover,:focus {
+    color: #ffffff;
+    background: #008cff;
+    border: 1px solid #008cff;
+    text-shadow: 0 0 5px #ffffff,
+                0 0 10px #ffffff,
+                0 0 20px #ffffff;
+    box-shadow: 0 0 5px #008cff,
+                0 0 20px #008cff,
+                0 0 50px #008cff,
+                0 0 100px #008cff;
+    cursor: pointer;
+    }
+
+`
 
     return(
         <ButtonsWrapper>
