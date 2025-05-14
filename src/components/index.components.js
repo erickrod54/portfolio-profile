e47ff96eeac6a049f8d6484e00da1cb0a4027ffa
Erick Rodriguez <erickrod54@gmail.com>
2025-05-13 21:32:00 -0500

    Portfolio-erick - version 19.05 - index > components - Features: Clearing HeaderButtons import

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,15 +1,14 @@
 import Footer from "./footer.component";
-import HeaderButtons from "./header.buttons.component";
 import Card from "./card.component";
 import AvatarPicture from "./avatar.component";
 import SocialComponent from "./social.component";
 import IconComponent from './icon.component'
 import OuterCircleIcons from "./outer.circle.icons.component";
 
-/**Portfolio-erick - version 19.04 - index > components
+/**Portfolio-erick - version 19.05 - index > components
  *  - Features:
  * 
- *      --> Clearing 'HeaderButtons'.
+ *      --> Clearing 'HeaderButtons' import.
  * 
  * Notes: This component now is on legacy directory
  */
