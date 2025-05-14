2d5b734e84d760192f049d850ac00d0243aa8ba1
Erick Rodriguez <erickrod54@gmail.com>
2025-05-13 21:34:00 -0500

    Portfolio-erick - version 19.07 - styled.compoennts - Features: Clearing FooterContainer

--- src/styled-components/styled.components.js
+++ src/styled-components/styled.components.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled, { css } from "styled-components"
 
-/**Portfolio-erick - version 19.01 - styled.components - Features:
+/**Portfolio-erick - version 19.07 - styled.components - Features:
  * 
- *      --> Clearing 'EachBtn'
+ *      --> Clearing 'FooterContainer'
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -453,18 +453,3 @@ export const CenterList = styled.ul`
 export const FooterCenter = styled.div`
     justify-self: end;
 ` 
-export const FooterContainer = styled.section`
-        display: grid;
-        margin: 2rem;
-        padding: 2rem;
-        grid-template-columns: repeat(2, 1fr); /* divide container into 3 equal columns */
-        grid-column: container-start / container-end;
-        background: linear-gradient(
-            to left,
-            rgb(48, 7, 82) 0%,
-            rgba(0, 128, 128, 1) 100%
-        );
-        border-radius: 2rem;
-        margin-left: 4.3rem;
-        margin-right: 4.3rem;
-`
