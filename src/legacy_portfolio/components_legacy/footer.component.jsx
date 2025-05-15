f97379daeee9f7213b4be4cc914a6b801317c913
Erick Rodriguez <erickrod54@gmail.com>
2025-05-14 21:30:00 -0500

    Portfolio-erick - version 19.07 - Footer - Features: Migrating back FooterContainer

--- src/legacy_portfolio/components_legacy/footer.component.jsx
+++ src/legacy_portfolio/components_legacy/footer.component.jsx
@@ -1,9 +1,10 @@
+import styled from "styled-components";
 import { usePortfolioContext } from "../context";
 import { AnchorTag, CenterList, CenterListItem, FooterCenter, FooterContainer, FooterRight } from "../styled-components/styled.components";
 
-/**Portfolio-erick - version 19.04 - Footer - Features:
+/**Portfolio-erick - version 19.07 - Footer - Features:
  * 
- *      --> Moving 'Footer' to legacy code
+ *      --> Migrating back 'FooterContainer' 
  * 
  * Notes: This components is adapted to the new layout
  * and is functional working, pending for layout the 
@@ -13,6 +14,22 @@ import { AnchorTag, CenterList, CenterListItem, FooterCenter, FooterContainer, F
 const Footer = () => {
 
     const { social } = usePortfolioContext()
+
+    const FooterContainer = styled.section`
+        display: grid;
+        margin: 2rem;
+        padding: 2rem;
+        grid-template-columns: repeat(2, 1fr); /* divide container into 3 equal columns */
+        grid-column: container-start / container-end;
+        background: linear-gradient(
+            to left,
+            rgb(48, 7, 82) 0%,
+            rgba(0, 128, 128, 1) 100%
+        );
+        border-radius: 2rem;
+        margin-left: 4.3rem;
+        margin-right: 4.3rem;
+`
 
     return(
         <FooterContainer>
