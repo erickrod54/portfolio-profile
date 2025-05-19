d7237335ab7b9e987cf62aa7ed6a6f011c83a287
Erick Rodriguez <erickrod54@gmail.com>
2025-05-18 21:33:00 -0500

    Portfolio-erick - version 20.04 - index > components - Features: Adding HeaderTitle

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -3,16 +3,18 @@ import AvatarPicture from "./avatar.component";
 import SocialComponent from "./social.component";
 import IconComponent from './icon.component'
 import OuterCircleIcons from "./outer.circle.icons.component";
+import HeaderTitle from "./header.title.component";
 
-/**Portfolio-erick - version 19.06 - index > components
+/**Portfolio-erick - version 20.04 - index > components
  *  - Features:
  * 
- *      --> Clearing 'Footer'.
+ *      --> Adding 'HeaderTitle'.
  * 
  * Notes: This component now is on legacy directory
  */
 
 export { SocialComponent,
+         HeaderTitle,   
          IconComponent,
          OuterCircleIcons,
          AvatarPicture, Card }
