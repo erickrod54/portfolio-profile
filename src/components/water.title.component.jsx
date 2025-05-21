41454aca9286381fa6bbb666e80d824498926cdf
Erick Rodriguez <erickrod54@gmail.com>
2025-05-20 21:30:00 -0500

    Portfolio-erick - version 20.08 - WaterTitle - Features: Building WaterTitle

---
+++ src/components/water.title.component.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+/**Portfolio-erick - version 20.08 - WaterTitle  -
+ * Features:
+ * 
+ *      --> Building 'WaterTitle'
+ * 
+ * Notes: To substite the legacy body implementation.
+ */
+
+
+const WaterTitle = ({ title }) => {
+
+  return (
+    <StyledWrapper> 
+      <div class="loader">
+          <span>{title}</span>
+          <span>{title}</span>
+      </div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div` 
+.loader {
+  position: relative;
+}
+
+.loader span {
+  position: absolute;
+  color: #fff;
+  transform: translate(-50%, -50%);
+  font-size: 38px;
+  letter-spacing: 5px;
+}
+
+.loader span:nth-child(1) {
+  color: transparent;
+  -webkit-text-stroke: 0.3px rgb(128, 198, 255);
+}
+
+.loader span:nth-child(2) {
+  color: rgb(128, 198, 255);
+  -webkit-text-stroke: 1px rgb(128, 198, 255);
+  animation: uiverse723 3s ease-in-out infinite;
+}
+
+@keyframes uiverse723 {
+  0%, 100% {
+    clip-path: polygon(0% 45%, 15% 44%, 32% 50%, 
+     54% 60%, 70% 61%, 84% 59%, 100% 52%, 100% 100%, 0% 100%);
+  }
+
+  50% {
+    clip-path: polygon(0% 60%, 16% 65%, 34% 66%, 
+     51% 62%, 67% 50%, 84% 45%, 100% 46%, 100% 100%, 0% 100%);
+  }
+}`;
+
+export default WaterTitle;
