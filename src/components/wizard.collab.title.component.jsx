1b921f72b3caf6fea9eabda3984d9cdef1ebaed0
Erick Rodriguez <erickrod54@gmail.com>
2025-05-22 21:30:00 -0500

    Portfolio-erick - version 20.15 - WizardCollabTitle - Features: Placing WaterTitleData

--- src/components/wizard.collab.title.component.jsx
+++ src/components/wizard.collab.title.component.jsx
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 import { WaterTitle } from "./index.components";
 
-/**Portfolio-erick - version 20.12 - WizardCollabTitle - Features:
+/**Portfolio-erick - version 20.15 - WizardCollabTitle - Features:
  * 
- *      --> Changing test to 'water'
+ *      --> Placing 'WaterTitleData'
  * 
  * Notes: This is the component title for open source collab 
  * section
  */
 
-/** Styled Components for new Portfolio - START */
+
+const WaterTitleData = [
+  { id: 1, title: 'Open' },
+  { id: 2, title: 'Sourcerer' }
+];
+
+// Destructure the second element (index 1) of the array - by leaving the 
+// comma - before of that is the index 0 and so on after 1 will be 2..
+const [{ title: firstTitle }, { title: secondTitle }] = WaterTitleData;
+
 
 const WizardCollabWrapper = styled.div`
 
