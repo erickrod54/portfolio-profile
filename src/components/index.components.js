fae73aea4bd73f9e4142c5574f67faea28ebd690
Erick Rodriguez <erickrod54@gmail.com>
2025-05-22 21:35:00 -0500

    Portfolio-erick - version 20.19 - index > components - Features: Adding WaterTitle

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -4,14 +4,14 @@ import SocialComponent from "./social.component";
 import IconComponent from './icon.component'
 import OuterCircleIcons from "./outer.circle.icons.component";
 import HeaderTitle from "./header.title.component";
-import WaterTitle from "./collaborations.title.component";
+import WaterTitle from "./water.title.component";
 import WizardCollabTitle from "./wizard.collab.title.component";
 import SectionDivider from "./section.divider.component";
 
-/**Portfolio-erick - version 20.14 - index > components
+/**Portfolio-erick - version 20.19 - index > components
  *  - Features:
  * 
- *      --> Adding 'SectionDivider'.
+ *      --> Adding 'WaterTitle'.
  * 
  * Notes: This component now is on legacy directory
  */
