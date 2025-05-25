34283033ab9eb6ef6969dfdf69916f5ab12b40f3
Erick Rodriguez <erickrod54@gmail.com>
2025-05-24 21:31:00 -0500

    Portfolio-erick - version 21.04 - App js file - Features: Adding Collaborations and rendering children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { Collaborations, CollabTitle, GitChart } from "./components/index.components.js";
 import { HeaderTitle, WizardCollabTitle } from "./components/index.components.js";
 import { Card } from "./components/index.components.js";
 import { SectionDivider } from "./components/index.components.js";
 import { ExperimentalUIWrapper } from "./styled-components/styled.components.index.js";
 
-/**Portfolio-erick - version 20.20 - App  js file -
+/**Portfolio-erick - version 21.04 - App  js file -
  * Features:
  * 
- *      --> Adding 'WizardCollabTitle'
+ *      --> Adding 'Collaborations' and rendering children
  * 
  * Notes: To substite the legacy body implementation.
  */
@@ -18,6 +19,10 @@ function App() {
       <Card />
       <SectionDivider />
       <WizardCollabTitle />
+      <Collaborations>
+        <CollabTitle />
+        <GitChart />      
+      </Collaborations>
     </ExperimentalUIWrapper>
   );
 }
