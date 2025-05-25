a599e8b20abf4b5e1e2dbc8b6c593ab382de0590
Erick Rodriguez <erickrod54@gmail.com>
2025-05-24 21:30:00 -0500

    Portfolio-erick - version 21.04 - CollabTitle - Features: Building CollabTitle

---
+++ src/components/collaborations.title.component.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+/**Portfolio-erick - version 21.04 - CollabTitle - Features:
+ * 
+ *      --> Building 'CollabTitle' 
+ * 
+ * Notes: This is the component will be refactored to received 
+ * data from sources
+ */
+
+const CollabTitle = () => {
+
+  return (
+    <StyledWrapper> 
+      <div className="animation">Daily Contributions!</div>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+
+@keyframes typing {
+  from {
+    width: 0;
+  }
+}
+
+@keyframes blink-caret {
+  50% {
+    border-color: transparent;
+  }
+}
+
+.animation {
+  /* 1. Define the gradient as the background */
+  background: linear-gradient(
+    315deg,
+    #ff0058, /* Pink/Red color */
+    #03a9f4  /* Blue/Cyan color */
+  );
+  
+  /* 2. Clip the background only where there is text */
+  -webkit-background-clip: text;
+  background-clip: text;
+  
+  /* 3. Make the foreground text color transparent so the background shows through */
+  -webkit-text-fill-color: transparent;
+  color: transparent; /* Fallback for non-webkit browsers */
+
+  font: bold 200% Consolas, Monaco, monospace;
+  
+  /* The border-right will still be a solid color */
+  border-right: .1em solid black; 
+  
+  /* You can adjust the width based on the number of characters in the text "Text writing!" (13 characters including space and exclamation) */
+  width: 20ch; 
+  margin: 2em auto; /* Centering horizontally */
+  white-space: nowrap;
+  overflow: hidden;
+
+  /* Adjust steps(13, end) for 13 characters */
+  -webkit-animation: typing 5s steps(13, end),
+	           blink-caret .5s step-end infinite alternate;
+}
+`;
+
+export default CollabTitle;
+
