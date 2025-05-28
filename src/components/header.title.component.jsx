337db92b2ea631a61d5713a37290c0a4a5543ef9
Erick Rodriguez <erickrod54@gmail.com>
2025-05-27 21:30:00 -0500

    Portfolio-erick - version 21.16 - HeaderTitle - Features: Adding scrollToHeader

--- src/components/header.title.component.jsx
+++ src/components/header.title.component.jsx
@@ -1,45 +1,64 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
-/**Portfolio-erick - version 21.09 - HeaderTitle - Features:
+/**Portfolio-erick - version 21.16 - HeaderTitle - Features:
  * 
- *      --> Adding white space to 'StyledWrapper'
+ *      --> Adding 'scrollToHeader' and 
  * 
  * Notes: This White space will be above '@erickdev' title
+ * 'scrollToHeader' will handle to make '@erickdev' title
+ * as the first visible element when the page loads
  */
 
 const HeaderTitle = () => {
+  const [isTextFilled, setIsTextFilled] = useState(true);
+  const headerRef = useRef(null);
 
-  const [ isTextFilled, setIsTextFilled ] = useState(true);
-  
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsTextFilled(false)
-    },6000)
-    
-    return () => clearTimeout(timer);
-    
-  }, [])
-   
+      setIsTextFilled(false);
+    }, 6000);
+
+    // Scroll behavior to ensure @erickdev is top visible
+    const scrollToHeader = () => {
+      if (headerRef.current) {
+        headerRef.current.scrollIntoView({
+          behavior: 'instant', // use 'smooth' if you prefer animation
+          block: 'start',
+        });
+      }
+    };
+
+    // Wait for the page layout to stabilize, then scroll
+    const raf = requestAnimationFrame(scrollToHeader);
+    const delay = setTimeout(scrollToHeader, 400);
+
+    return () => {
+      clearTimeout(timer);
+      cancelAnimationFrame(raf);
+      clearTimeout(delay);
+    };
+  }, []);
+
   return (
-    <StyledWrapper $isFilled={isTextFilled}>
+    <StyledWrapper ref={headerRef} $isFilled={isTextFilled}>
       <button className="button" data-text="Awesome">
         <span className="actual-text">&nbsp;@erickdev&nbsp;</span>
         <span aria-hidden="true" className="front-text">&nbsp;@erickdev&nbsp;</span>
       </button>
     </StyledWrapper>
   );
-}
+};
 
 const StyledWrapper = styled.div`
-  padding-top:4rem;
+  padding-top: 4rem;
   .button {
     position: relative;
     border: none;
     background: transparent;
     --stroke-color: #ffffff7c;
     --ani-color: rgba(95, 3, 244, 0);
-    --color-gar: linear-gradient(90deg,#03a9f4,#f441a5,#ffeb3b,#03a9f4);
+    --color-gar: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
     letter-spacing: 3px;
     font-size: 2em;
     font-family: "Arial";
@@ -64,13 +83,15 @@ const StyledWrapper = styled.div`
     border-bottom: 2px solid transparent;
   }
 
-   ${props => props.$isFilled && ` 
+  ${props =>
+    props.$isFilled &&
+    `
     .front-text {
       width: 100%;
       border-bottom: 2px solid #03a9f4;
       -webkit-text-stroke: 1px var(--ani-color);
     }
-    `}
+  `}
 
   .button:hover .front-text {
     width: 100%;
@@ -82,14 +103,13 @@ const StyledWrapper = styled.div`
     0% {
       background-position: 0%;
     }
-
     50% {
       background-position: 400%;
     }
-
     100% {
       background-position: 0%;
     }
-  }`;
+  }
+`;
 
 export default HeaderTitle;
