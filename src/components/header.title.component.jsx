6b36f7e54da7e71610aae4c34abf23089628488d
Erick Rodriguez <erickrod54@gmail.com>
2025-05-28 21:33:00 -0500

    Portfolio-erick - version 22.04 - HeaderTitle - Features: Removing data-text=erickdev

--- src/components/header.title.component.jsx
+++ src/components/header.title.component.jsx
@@ -1,21 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import styled, { css, keyframes } from 'styled-components';
 
-/**Portfolio-erick - version 22.03 - HeaderTitle - Features:
+/**Portfolio-erick - version 22.04 - HeaderTitle - Features:
  * 
- *      --> Adding 'aria-hidden="true"'
+ *      --> Removing 'data-text=erickdev'
  * 
- * Notes: This attribute tells assistive technologies 
- * (like screen readers) to ignore the element and 
- * its children. 
- * 
- * Since both the actual-text and the
- * front-text spans contain the exact same text 
- * ("@erickdev"), a screen reader would normally read 
- * the text twice. The aria-hidden="true" on the 
- * animated copy ensures that the user only hears 
- * the title once, which is necessary for 
- * a good user experience.
+ * Notes: None of your current CSS or styled components 
+ * are referencing or using the data-text attribute
  */
 
 const HeaderTitle = () => {
@@ -50,7 +41,7 @@ const HeaderTitle = () => {
 
   return (
     <StyledWrapper ref={headerRef}>
-      <ButtonTitle data-text="@erickdev">
+      <ButtonTitle>
         <FrontText $isFilled={isTextFilled}>&nbsp;@erickdev&nbsp;</FrontText>
         <BackText aria-hidden="true" >&nbsp;@erickdev&nbsp;</BackText>
       </ButtonTitle>
