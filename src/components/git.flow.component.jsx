a1965d8029241e47f1f63bc2d063d8e075f0f643
Erick Rodriguez <erickrod54@gmail.com>
2025-06-02 21:33:00 -0500

    Portfolio-erick - version 23.10 - GitFlowComponent - Features: Replacing third TabIcon

--- src/components/git.flow.component.jsx
+++ src/components/git.flow.component.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
-/**Portfolio-erick - version 23.09 - GitFlowComponent - Features:
+/**Portfolio-erick - version 23.10 - GitFlowComponent - Features:
  * 
- *      --> Replacing second 'tab-text' to 'TabText'
+ *      --> Replacing third 'TabIcon'
  * 
  * Notes: these are the icons on the top right corner
  */
@@ -664,13 +664,13 @@ const GitFlowComponent = () => {
                 <TabText>Issues</TabText>
                 </div>
                 <div class="tab active">
-                <div class="tab-icon">
+                <TabIcon>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                     <path
                         d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"
                     ></path>
                     </svg>
-                </div>
+                </TabIcon>
                 <div class="tab-text">Pull Requests</div>
                 </div>
             </div>
