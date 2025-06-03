228bb073c59dc8d3e0cd0d68360786f94df7a5e4
Erick Rodriguez <erickrod54@gmail.com>
2025-06-02 21:31:00 -0500

    Portfolio-erick - version 23.08 - GitFlowComponent - Features: Replacing second TabIcon

--- src/components/git.flow.component.jsx
+++ src/components/git.flow.component.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
-/**Portfolio-erick - version 23.07 - GitFlowComponent - Features:
+/**Portfolio-erick - version 23.08 - GitFlowComponent - Features:
  * 
- *      --> Replacing first 'tab-text' to 'TabText'  
+ *      --> Replacing second 'TabIcon'
  * 
  * Notes: these are the icons on the top right corner
  */
@@ -653,14 +653,14 @@ const GitFlowComponent = () => {
                 <TabText>Code</TabText>
                 </div>
                 <div class="tab">
-                <div class="tab-icon">
+                <TabIcon>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                     <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
                     <path
                         d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"
                     ></path>
                     </svg>
-                </div>
+                </TabIcon>
                 <div class="tab-text">Issues</div>
                 </div>
                 <div class="tab active">
