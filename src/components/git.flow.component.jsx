410ae6532271702130f01c113ff61231d40ec28d
Erick Rodriguez <erickrod54@gmail.com>
2025-06-03 21:30:00 -0500

    Portfolio-erick - version 23.13 - GitFlowComponent - Features: Replacing second tab to TabStyled

--- src/components/git.flow.component.jsx
+++ src/components/git.flow.component.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
 
-/**Portfolio-erick - version 23.12 - GitFlowComponent - Features:
+/**Portfolio-erick - version 23.13 - GitFlowComponent - Features:
  * 
- *      --> Replacing first 'tab' to 'TabStyled'
+ *      --> Replacing second 'tab' to 'TabStyled'
  * 
  * Notes: these are the icons on the top right corner
  */
@@ -681,7 +681,7 @@ const GitFlowComponent = () => {
                 </TabIcon>
                 <TabText>Code</TabText>
                 </TabStyled>
-                <div class="tab">
+                <TabStyled>
                 <TabIcon>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                     <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
@@ -691,7 +691,7 @@ const GitFlowComponent = () => {
                     </svg>
                 </TabIcon>
                 <TabText>Issues</TabText>
-                </div>
+                </TabStyled>
                 <div class="tab active">
                 <TabIcon>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
