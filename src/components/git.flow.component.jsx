4c835e412fe0d3c3832a498a0523e3c4c9a2fe1c
Erick Rodriguez <erickrod54@gmail.com>
2025-06-06 21:30:00 -0500

    Portfolio-erick - version 24.10 - GitFlowComponent - Features: Placing fourth CheckboxWrapper

--- src/components/git.flow.component.jsx
+++ src/components/git.flow.component.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import styled, { css } from "styled-components";
 
-/**Portfolio-erick - version 24.09 - GitFlowComponent - Features:
+/**Portfolio-erick - version 24.10 - GitFlowComponent - Features:
  * 
- *      --> Placing third 'label'
+ *      --> Placing fourth 'CheckboxWrapper'
  * 
  * Notes: 'HiddenCheckbox' is the actual input
  * 
@@ -849,8 +849,10 @@ const GitFlowComponent = () => {
                 </div>
                 <div class="pr">
                 <label>
-                    <input type="checkbox" />
-                    <div class="checkbox"></div>
+                    <CheckboxWrapper>
+                        <input type="checkbox" />
+                        <div class="checkbox"></div>
+                    </CheckboxWrapper>
                 </label>
                 <div class="pr-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
