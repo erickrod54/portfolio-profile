8e5fa8830cc4d4dda1b8347fb85e20b31639e644
Erick Rodriguez <erickrod54@gmail.com>
2025-06-24 21:34:00 -0500

    Portfolio-erick - version 29.17 - GitFlowComponent - Features: Building burgermenuData

--- src/components/git.flow.component.jsx
+++ src/components/git.flow.component.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import styled, { css, keyframes } from "styled-components";
 
-/**Portfolio-erick - version 29.16 - GitFlowComponent - Features:
+/**Portfolio-erick - version 29.17 - GitFlowComponent - Features:
  * 
- *      --> Removing 'pfp' from 'GitFlowWrapper'  
+ *      --> Building 'burgerMenuData'  
  * 
  * Notes: 'HiddenCheckbox' is the actual input
  * 
@@ -413,6 +413,19 @@ const GitFlowWrapper = styled.div`
 
 `
 
+export const burgerMenuData = [
+  {
+    xmlns: 'http://www.w3.org/2000/svg',
+    viewBox: '0 0 16 16',
+    width: '16',
+    height: '16',
+  },
+  {
+    fill: '',
+    d: 'M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z'
+  },
+];
+
 const GitFlowComponent = () => {
 
     const [checked, setChecked] = useState(false);
@@ -460,6 +473,7 @@ const GitFlowComponent = () => {
         <GitFlowCardBox>
             <HeaderBox>
                 <TopHeader>
+                    {/**burger menu */}
                     <Icon>
                     <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                         <path
