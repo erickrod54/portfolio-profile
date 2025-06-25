800550dc852001486f7e44c75c8825ff2f998b1d
Erick Rodriguez <erickrod54@gmail.com>
2025-06-24 21:35:00 -0500

    Portfolio-erick - version 29.18 - GitFlowComponent - Features: Implementing IconComponent with burgerMenuData

--- src/components/git.flow.component.jsx
+++ src/components/git.flow.component.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import styled, { css, keyframes } from "styled-components";
+import { IconComponent } from './index.components';
 
-/**Portfolio-erick - version 29.17 - GitFlowComponent - Features:
+/**Portfolio-erick - version 29.18 - GitFlowComponent - Features:
  * 
- *      --> Building 'burgerMenuData'  
+ *      --> Implementing 'IconComponent' with 'burgerMenuData'  
  * 
  * Notes: 'HiddenCheckbox' is the actual input
  * 
@@ -475,11 +476,7 @@ const GitFlowComponent = () => {
                 <TopHeader>
                     {/**burger menu */}
                     <Icon>
-                    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                        d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"
-                        ></path>
-                    </svg>
+                        <IconComponent data={burgerMenuData} />
                     </Icon>
                     <GhIcon>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
