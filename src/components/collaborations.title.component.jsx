4c7b426a541f76c0345d1dc2177a1cd063ee24ed
Erick Rodriguez <erickrod54@gmail.com>
2025-07-04 21:30:00 -0500

    Portfolio-erick - version 32.05 - CollabTitle - Features: Building AnimationWrapper

--- src/components/collaborations.title.component.jsx
+++ src/components/collaborations.title.component.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 import { BlinkCaretKeyframe, TypingKeyFrame } from '../styled-components/styled.components';
 
-/**Portfolio-erick - version 32.04 - CollabTitle - Features:
+/**Portfolio-erick - version 32.05 - CollabTitle - Features:
  * 
- *      --> Clearing 'keyframes' helper  
+ *      --> Building 'AnimationWrapper' styles   
  * 
  * Notes: 'CollabTitleData' is going to be placed where the 
  * 'CollabTitle' is going to consume it.
@@ -13,34 +13,32 @@ const CollabTitle = ({title}) => {
 
   return (
     <StyledWrapper> 
-      <div className="animation">{title}</div>
+      <AnimationWrapper>{title}</AnimationWrapper>
     </StyledWrapper>
   );
 }
 
-const StyledWrapper = styled.div`
-
-.animation {
+const AnimationWrapper = styled.div`
   /* 1. Define the gradient as the background */
   background: linear-gradient(
     315deg,
     #ff0058, /* Pink/Red color */
     #03a9f4  /* Blue/Cyan color */
   );
-  
+
   /* 2. Clip the background only where there is text */
   -webkit-background-clip: text;
   background-clip: text;
-  
+
   /* 3. Make the foreground text color transparent so the background shows through */
   -webkit-text-fill-color: transparent;
   color: transparent; /* Fallback for non-webkit browsers */
 
   font: bold 200% Consolas, Monaco, monospace;
-  
+
   /* The border-right will still be a solid color */
   border-right: .1em solid white; 
-  
+
   /* You can adjust the width based on the number of characters in the text "Text writing!" (13 characters including space and exclamation) */
   width: 20ch; 
   margin: 2em auto; /* Centering horizontally */
@@ -49,8 +47,11 @@ const StyledWrapper = styled.div`
 
   /* Adjust steps(13, end) for 13 characters */
   -webkit-animation: ${TypingKeyFrame} 5s steps(13, end),
-	           ${BlinkCaretKeyframe} .5s step-end infinite alternate;
-}
+            ${BlinkCaretKeyframe} .5s step-end infinite alternate;
+
+`
+
+const StyledWrapper = styled.div`
 `;
 
 export default CollabTitle;
