6cced67e66732326c80ade58ba25933dc6061336
Erick Rodriguez <erickrod54@gmail.com>
2025-07-04 21:33:00 -0500

    Portfolio-erick - version 32.05 - CollabTitle - Features: Migrating AnimationWrapper styles

--- src/components/collaborations.title.component.jsx
+++ src/components/collaborations.title.component.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import { BlinkCaretKeyframe, TypingKeyFrame } from '../styled-components/styled.components';
+import { BlinkCaretKeyframe, TypingKeyFrame, AnimationWrapper } from '../styled-components/styled.components';
 
 /**Portfolio-erick - version 32.05 - CollabTitle - Features:
  * 
- *      --> Building 'AnimationWrapper' styles   
+ *      --> Migrating 'AnimationWrapper' styles   
  * 
  * Notes: 'CollabTitleData' is going to be placed where the 
  * 'CollabTitle' is going to consume it.
@@ -17,39 +17,6 @@ const CollabTitle = ({title}) => {
     </StyledWrapper>
   );
 }
-
-const AnimationWrapper = styled.div`
-  /* 1. Define the gradient as the background */
-  background: linear-gradient(
-    315deg,
-    #ff0058, /* Pink/Red color */
-    #03a9f4  /* Blue/Cyan color */
-  );
-
-  /* 2. Clip the background only where there is text */
-  -webkit-background-clip: text;
-  background-clip: text;
-
-  /* 3. Make the foreground text color transparent so the background shows through */
-  -webkit-text-fill-color: transparent;
-  color: transparent; /* Fallback for non-webkit browsers */
-
-  font: bold 200% Consolas, Monaco, monospace;
-
-  /* The border-right will still be a solid color */
-  border-right: .1em solid white; 
-
-  /* You can adjust the width based on the number of characters in the text "Text writing!" (13 characters including space and exclamation) */
-  width: 20ch; 
-  margin: 2em auto; /* Centering horizontally */
-  white-space: nowrap;
-  overflow: hidden;
-
-  /* Adjust steps(13, end) for 13 characters */
-  -webkit-animation: ${TypingKeyFrame} 5s steps(13, end),
-            ${BlinkCaretKeyframe} .5s step-end infinite alternate;
-
-`
 
 const StyledWrapper = styled.div`
 `;
