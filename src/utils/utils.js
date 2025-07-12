6cc9839abf4dbb8581f7f655638852934e04e88a
Erick Rodriguez <erickrod54@gmail.com>
2025-07-11 21:33:00 -0500

    Portfolio-erick - version 34.01 - utils - Features: Building getCurrentYear

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+/**Portfolio-erick - version 34.01 - utils - Features:
+ * 
+ *      --> Building 'getCurrentYear'
+ * 
+ * Notes: This component receives 'children' components
+ */
+
+/**javascript util that get the full current year */
+export const getCurrentYear = () => {
+    return new Date().getFullYear();
+}
