ec708fda9c0c924e4a19a475c84b4aa14edd8946
Erick Rodriguez <erickrod54@gmail.com>
2025-07-23 21:32:00 -0500

    Portfolio-erick - version 35.09 - GitChart - Features: Migrating CommitCard

--- src/components/git.chart.component.jsx
+++ src/components/git.chart.component.jsx
@@ -1,33 +1,13 @@
 import styled, { css, keyframes } from "styled-components"
-import { MainChart, loading90 } from '../styled-components/styled.components.index';
+import { MainChart, loading90, CommitCard } from '../styled-components/styled.components.index';
 
-/**Portfolio-erick - version 35.08 - GitChart - Features:
+/**Portfolio-erick - version 35.09 - GitChart - Features:
  * 
- *      --> Migrating 'MainChart' 
+ *      --> Migrating 'CommitCard' 
  * 
  * Notes: This component soon will be refactored to work more efficent
  * and hold more complete information
  */
-
-const CommitCard = styled.div`
-  width: 40px;
-  height: 40px;
-  border-top-left-radius: 10px;
-  background: lightgrey;
-  transition: 0.4s ease-in-out, 0.2s background-color;
-  background: rgba(255, 255, 255, 0.2);
-  backdrop-filter: blur(5px);
-  margin: 0.2em;
-  border-radius: 5px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  border: 1px solid rgba(255, 255, 255, 0.3);
-  animation: ${loading90} infinite;
-
-  ${props => props.$index % 2 === 0 && css`
-    animation-delay:1s
-    `};
-
-`
 
 const GitChartWrapper = styled.div` 
 
