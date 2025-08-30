b45f183dde2d3face8beac9e511f6113758af1de
Erick Rodriguez <erickrod54@gmail.com>
2025-08-29 21:34:00 -0500

    Portfolio-erick - version 40.02 - WaterTitle - Features: Splitting Universe723 keyframes and injecting css into Loader

--- src/components/water.title.component.jsx
+++ src/components/water.title.component.jsx
@@ -1,9 +1,10 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-/**Portfolio-erick - version 40.01 - WaterTitle  -
+/**Portfolio-erick - version 40.02 - WaterTitle  -
  * Features:
  * 
- *      --> Building 'Loader'
+ *      --> Splitting 'Universe723' keyframes 
+ *          and injecting css into Loader
  * 
  * Notes: This data is to test WaterTitle, the data
  * will be relocated or provided to the component
@@ -21,6 +22,19 @@ const WaterTitle = ({ title }) => {
     </WaterTitleWrapper>
   );
 }
+
+
+const Universe723 = keyframes`
+    0%, 100% {
+      clip-path: polygon(0% 45%, 15% 44%, 32% 50%, 
+      54% 60%, 70% 61%, 84% 59%, 100% 52%, 100% 100%, 0% 100%);
+    }
+
+    50% {
+      clip-path: polygon(0% 60%, 16% 65%, 34% 66%, 
+      51% 62%, 67% 50%, 84% 45%, 100% 46%, 100% 100%, 0% 100%);
+    }
+`
 
 /**the loader class is converted to a component the span are marked 
  * as direct child of the Loader styled component by '>'*/
@@ -48,7 +62,7 @@ const Loader = styled.div`
   & > span:nth-child(2) {
   color: rgb(128, 198, 255);
   -webkit-text-stroke: 1px rgb(128, 198, 255);
-  animation: uiverse723 3s ease-in-out infinite;
+  animation: ${Universe723} 3s ease-in-out infinite;
   }
 `
 
@@ -57,16 +71,6 @@ const WaterTitleWrapper = styled.div`
   align-self: center;
 
 
-@keyframes uiverse723 {
-  0%, 100% {
-    clip-path: polygon(0% 45%, 15% 44%, 32% 50%, 
-     54% 60%, 70% 61%, 84% 59%, 100% 52%, 100% 100%, 0% 100%);
-  }
-
-  50% {
-    clip-path: polygon(0% 60%, 16% 65%, 34% 66%, 
-     51% 62%, 67% 50%, 84% 45%, 100% 46%, 100% 100%, 0% 100%);
-  }
-}`;
+`;
 
 export default WaterTitle;
