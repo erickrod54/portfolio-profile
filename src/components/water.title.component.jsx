e11a54a170313dc530b7df397a32688066014f59
Erick Rodriguez <erickrod54@gmail.com>
2025-08-29 21:33:00 -0500

    Portfolio-erick - version 40.01 - WaterTitle - Features: Building Loader

--- src/components/water.title.component.jsx
+++ src/components/water.title.component.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-/**Portfolio-erick - version 40.00 - WaterTitle  -
+/**Portfolio-erick - version 40.01 - WaterTitle  -
  * Features:
  * 
- *      --> Renaming 'StyledWrapper' to 'WaterTitleWrapper'
+ *      --> Building 'Loader'
  * 
  * Notes: This data is to test WaterTitle, the data
  * will be relocated or provided to the component
@@ -14,25 +14,22 @@ const WaterTitle = ({ title }) => {
 
   return (
     <WaterTitleWrapper> 
-      <div class="loader">
+      <Loader>
           <span>{title}</span>
           <span>{title}</span>
-      </div>
+      </Loader>
     </WaterTitleWrapper>
   );
 }
 
-const WaterTitleWrapper = styled.div` 
-  width: auto;
-  align-self: center;
-
-  .loader {
+/**the loader class is converted to a component the span are marked 
+ * as direct child of the Loader styled component by '>'*/
+const Loader = styled.div`
     position: relative; 
     width: 140px; 
     height: 60px; 
-  }
-
-  .loader span {
+  
+  & > span {
     position: absolute;
     color: #fff;
     /* Keep centering relative to the .loader div */
@@ -43,16 +40,22 @@ const WaterTitleWrapper = styled.div`
     letter-spacing: 5px;
   }
 
-.loader span:nth-child(1) {
+  & > span:nth-child(1) {
   color: transparent;
   -webkit-text-stroke: 0.3px rgb(128, 198, 255);
-}
+  }
 
-.loader span:nth-child(2) {
+  & > span:nth-child(2) {
   color: rgb(128, 198, 255);
   -webkit-text-stroke: 1px rgb(128, 198, 255);
   animation: uiverse723 3s ease-in-out infinite;
-}
+  }
+`
+
+const WaterTitleWrapper = styled.div` 
+  width: auto;
+  align-self: center;
+
 
 @keyframes uiverse723 {
   0%, 100% {
