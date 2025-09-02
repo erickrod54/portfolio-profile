ab69947b4c2e0574e14e5f35db854dd4fac5ee7b
Erick Rodriguez <erickrod54@gmail.com>
2025-09-01 21:34:00 -0500

    Portfolio-erick - version 40.09 - WaterTitle - Features: Migrating Loader

--- src/components/water.title.component.jsx
+++ src/components/water.title.component.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
-import { TitleSpan, WaterTitleWrapper, AlterTitleSpan, Universe723 } from '../styled-components/styled.components.index';
+import { TitleSpan, WaterTitleWrapper, AlterTitleSpan, Universe723, Loader } from '../styled-components/styled.components.index';
 
-/**Portfolio-erick - version 40.08 - WaterTitle  -
+/**Portfolio-erick - version 40.09 - WaterTitle  -
  * Features:
  * 
- *      --> Clearing 'keyframes'
+ *      --> Migrating 'Loader'
  * 
  * Notes: This data is to test WaterTitle, the data
  * will be relocated or provided to the component
@@ -22,35 +22,5 @@ const WaterTitle = ({ title }) => {
     </WaterTitleWrapper>
   );
 }
-
-/**the loader class is converted to a component the span are marked 
- * as direct child of the Loader styled component by '>'*/
-const Loader = styled.div`
-    position: relative; 
-    width: 140px; 
-    height: 60px; 
-  
-  & > span {
-    position: absolute;
-    color: #fff;
-    /* Keep centering relative to the .loader div */
-    top: 50%;
-    left: 50%; 
-    transform: translate(-50%, -50%);
-    font-size: 38px;
-    letter-spacing: 5px;
-  }
-
-  & > span:nth-child(1) {
-  color: transparent;
-  -webkit-text-stroke: 0.3px rgb(128, 198, 255);
-  }
-
-  & > span:nth-child(2) {
-  color: rgb(128, 198, 255);
-  -webkit-text-stroke: 1px rgb(128, 198, 255);
-  animation: ${Universe723} 3s ease-in-out infinite;
-  }
-`
 
 export default WaterTitle;
