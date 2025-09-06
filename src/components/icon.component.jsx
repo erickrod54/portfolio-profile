e9f32fd8f491f397dce34b56057cba91914a0933
Erick Rodriguez <erickrod54@gmail.com>
2025-09-05 21:34:00 -0500

    Portfolio-erick - version 41.06 - IconComponent - Features: Rules to receive gradients stops and offsets

--- src/components/icon.component.jsx
+++ src/components/icon.component.jsx
@@ -1,9 +1,9 @@
 import { IconComponentWrapper } from '../styled-components/styled.components.index';
 /**
- * Portfolio-erick - version 41.05 - IconComponent 
+ * Portfolio-erick - version 41.06 - IconComponent 
  * Features:
  * 
- *   --> Building 'hasGradients'.
+ *   --> Rules to receive 'gradients, stops, and offsets'.
  * 
  * Note: This component is the base component for a 
  * generic SVG icon component
@@ -28,6 +28,29 @@ const IconComponent = ({ data }) => {
       width={svgProps.width}
       height={svgProps.height}
     >
+      {hasGradients && (
+        <defs>
+          {svgProps.gradients.map((gradient, gIndex) => (
+            <linearGradient 
+              key={gIndex}
+              id={gradient.id}
+              x1={gradient.x1 || '0%'} //Default to 0% if not provided
+              y1={gradient.y1 || '0%'} //Default to 0% if not provided
+              x2={gradient.x2 || '100%'} //Default to 100% if not provided
+              y2={gradient.y2 || '100%'} //Default to 100% if not provided   
+            >
+              {/**rendering stop attribs */}
+              {gradient.stops.map((stop, sIndex) => (
+                <stop>
+                  key={sIndex}
+                  offset={stop.offset}
+                  style={{ stopColor: stop.color, stopOpacity: stop.opacity }}
+                </stop>
+              ))}
+            </linearGradient>
+          ))}
+        </defs>
+      )}
       {paths.map((path, index) => (
         <path
           key={index}
