8e769fb935c6b5dd45e35e308ca57bd99309b070
Erick Rodriguez <erickrod54@gmail.com>
2025-09-14 21:32:00 -0500

    Migrating old index to legacy code

---
+++ src/legacy_portfolio/pages/index.pages.js
@@ -0,0 +1,12 @@
+import HomePage from "./home.page";
+
+/**Portfolio-erick - version 6.06 - index > pages
+ *  - Features:
+ * 
+ *      --> Restructuring components in the pages > 
+ *          index.
+ * 
+ * Notes: More components will be added
+ */
+
+export { HomePage }
