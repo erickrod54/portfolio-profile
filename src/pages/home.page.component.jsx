42ea197a70d15377c49e3dc2f577af53cbbcd81a
Erick Rodriguez <erickrod54@gmail.com>
2025-09-14 21:35:00 -0500

    Portfolio-erick - version 43.09 - HomePageComponent - Features: Building HoemPageComponent

---
+++ src/pages/home.page.component.jsx
@@ -0,0 +1,20 @@
+import { } from "../components/index.components";
+
+/**Portfolio-erick - version 43.09 - HomePageComponent -
+ * Features:
+ * 
+ *      --> Building 'HomePageComponent'
+ * 
+ * Notes: This file is gonna be changed later.
+ */
+
+const HomePageComponent = () => {
+
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default HomePageComponent;
