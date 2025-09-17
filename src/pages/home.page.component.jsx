6713df906807888e35f3e0f683fc0c558950f065
Erick Rodriguez <erickrod54@gmail.com>
2025-09-16 21:32:00 -0500

    Portfolio-erick - version 43.18 - HomePageComponent - Features: Destructuring firstTitle from CollabTitleData

--- src/pages/home.page.component.jsx
+++ src/pages/home.page.component.jsx
@@ -1,18 +1,19 @@
 import { Card, Collaborations, CollabTitle, FloatCard, HeaderTitle, SectionDivider, WizardCollabTitle } from "../components/index.components";
 import { usePortfolioContext } from "../context";
 
-/**Portfolio-erick - version 43.17 - HomePageComponent -
+/**Portfolio-erick - version 43.18 - HomePageComponent -
  * Features:
  * 
- *      --> Destructuring 'CollabTitleData' from context
+ *      --> Destructuring 'firstTtile' from 'CollabTitleData'
  * 
  * Notes: This file is gonna be changed later.
 */
 
 const HomePageComponent = () => {
     
-    const { CollabTitleData } = usePortfolioContext()
+    const { CollabTitleData } = usePortfolioContext();
 
+    const [{ title: firstTtile },] = CollabTitleData;
     return(
         <>
          <HeaderTitle />
