b0619c16495ff0259c43d1e4169478fa3d17dbdf
Erick Rodriguez <erickrod54@gmail.com>
2025-09-17 21:35:00 -0500

    Portfolio-erick - version 44.06 - HomePage - Features: Importing and exporting HomePageComponent

---
+++ src/pages/index.pages.components.jsx
@@ -0,0 +1,12 @@
+import HomePageComponent from "./home.page.component";
+
+/**Portfolio-erick - version 44.06 - HomePage -
+ * Features:
+ * 
+ *      --> Importing and exporting 'HomePageComponent'
+ * 
+ * Notes: This file is gonna be changed later.
+*/
+
+
+export { HomePageComponent }
