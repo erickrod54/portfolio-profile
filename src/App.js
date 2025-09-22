52d412b3bc636aeff662aac3c8c29d4826eab9df
Erick Rodriguez <erickrod54@gmail.com>
2025-09-21 21:34:00 -0500

    Portfolio-erick - version 45.09 - App js - Features: Removing seventh Collaborations import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Collaborations, CollabTitle, Footer } from "./components/index.components.js";
+import { CollabTitle, Footer } from "./components/index.components.js";
 import { ExperimentalUIWrapper } from "./styled-components/styled.components.index.js";
 import { usePortfolioContext } from './context.js';
 import { Route, Switch } from "wouter";
 
-/**Portfolio-erick - version 45.08 - App  js file -
+/**Portfolio-erick - version 45.09 - App  js file -
  * Features:
  * 
- *      --> Removing seventh 'Collaborations'    
+ *      --> Removing seventh 'Collaborations' import     
  * 
  * Notes: To substite the legacy body implementation.
  */
