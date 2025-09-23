c8c13b73579f5c4e2e52b291ab7883d64212172d
Erick Rodriguez <erickrod54@gmail.com>
2025-09-22 21:35:00 -0500

    Portfolio-erick - version 45.16 - App js file - Features: Removing Footer import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { CollabTitle, Footer } from "./components/index.components.js";
+import { CollabTitle } from "./components/index.components.js";
 import { ExperimentalUIWrapper } from "./styled-components/styled.components.index.js";
 import { Route, Switch } from "wouter";
 
-/**Portfolio-erick - version 45.15 - App  js file -
+/**Portfolio-erick - version 45.16 - App  js file -
  * Features:
  * 
- *      --> Removing 'usePortfolioContext' import     
+ *      --> Removing 'Footer' import     
  * 
  * Notes: To substite the legacy body implementation.
  */
