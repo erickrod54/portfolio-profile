00aa4446a53b491a0eeac2093c1c1506c9144ceb
Erick Rodriguez <erickrod54@gmail.com>
2025-09-23 21:30:00 -0500

    Portfolio-profile - version 45.17 - UpdatingPageComponent - Features: Building UpdatingPageComponent

---
+++ src/pages/work.in.progress.page.component.jsx
@@ -0,0 +1,20 @@
+
+/**Portfolio-erick - version 45.17 - UpdatingPageComponent -
+ * Features:
+ * 
+ *      --> Building 'UpdatingPageComponent'     
+ * 
+ * Notes: To substite the legacy body implementation.
+ */
+
+const UpdatingPageComponent = () => {
+
+  return (
+    <>
+      <h1 style={{color: "white"}}>Updating - 😉 work in progress</h1> 
+    </>
+      
+  );
+}
+
+export default UpdatingPageComponent;
