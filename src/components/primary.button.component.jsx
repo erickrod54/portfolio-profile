dbba67797a53d7450b8af617cb34cb1de2ed28d3
Erick Rodriguez <erickrod54@gmail.com>
2025-09-26 21:31:00 -0500

    Portfolio-erick - version 46.14 - PrimaryButton - Features: Building PrimaryButton

---
+++ src/components/primary.button.component.jsx
@@ -0,0 +1,19 @@
+
+/**Portfolio-erick - version 46.14 - PrimaryButton -
+ * Features:
+ * 
+ *      --> Building 'PrimaryButton' 
+ * 
+ * Notes: To substite the legacy body implementation.
+ */
+
+const PrimaryButton = () => {
+
+    return( 
+    <button class="button">
+        <span>Go to Home</span>
+    </button>
+    )
+}
+
+export default PrimaryButton;
