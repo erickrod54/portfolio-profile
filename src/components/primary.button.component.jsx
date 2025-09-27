eb2438b7ee09e4f7fa66b487c29ba0ecc28e4e7b
Erick Rodriguez <erickrod54@gmail.com>
2025-09-26 21:33:00 -0500

    Portfolio-erick - version 46.15 - PrimaryButton - Features: Building StyledButton

--- src/components/primary.button.component.jsx
+++ src/components/primary.button.component.jsx
@@ -1,11 +1,59 @@
+import styled from "styled-components";
 
-/**Portfolio-erick - version 46.14 - PrimaryButton -
+/**Portfolio-erick - version 46.15 - PrimaryButton -
  * Features:
  * 
- *      --> Building 'PrimaryButton' 
+ *      --> Building 'StyledButton' 
  * 
  * Notes: To substite the legacy body implementation.
  */
+
+
+const StyledButton = styled.button`
+.button {
+  position: relative;
+  text-decoration: none;
+  color: #fff;
+  background: linear-gradient(45deg, #0ce39a, #69007f, #fc0987);
+  padding: 14px 25px;
+  border-radius: 10px;
+  font-size: 1.25em;
+  cursor: pointer;
+}
+
+.button span {
+  position: relative;
+  z-index: 1;
+}
+
+.button::before {
+  content: "";
+  position: absolute;
+  inset: 1px;
+  background: #272727;
+  border-radius: 9px;
+  transition: 0.5s;
+}
+
+.button:hover::before {
+  opacity: 0.7;
+}
+
+.button::after {
+  content: "";
+  position: absolute;
+  inset: 0px;
+  background: linear-gradient(45deg, #0ce39a, #69007f, #fc0987);
+  border-radius: 9px;
+  transition: 0.5s;
+  opacity: 0;
+  filter: blur(20px);
+}
+
+.button:hover:after {
+  opacity: 1;
+}
+`
 
 const PrimaryButton = () => {
 
