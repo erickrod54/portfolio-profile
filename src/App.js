53eb2c6c37b92db5357e4a9a258da05c750b8b1d
Erick Rodriguez <erickrod54@gmail.com>
2025-09-30 21:35:00 -0500

    Portfolio-erick - version 47.17 - App js file -  Features: Defining ninth route privacy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { HomePageComponent, UpdatingPageComponent } from "./pages/index.pages.co
 import { ExperimentalUIWrapper } from "./styled-components/styled.components.index.js";
 import { Route, Switch } from "wouter";
 
-/**Portfolio-erick - version 47.16 - App  js file -
+/**Portfolio-erick - version 47.17 - App  js file -
  * Features:
  * 
- *      --> Defining eighth route 'careers'     
+ *      --> Defining ninth route 'privacy'     
  * 
  * Notes: These routes are from 'AI & Classic Engineering 
  * Program'
@@ -44,6 +44,9 @@ function App() {
             <UpdatingPageComponent/>
         </Route>
         <Route path={'/careers'}>
+            <UpdatingPageComponent/>
+        </Route>
+        <Route path={'/privacy'}>
             <UpdatingPageComponent/>
         </Route>
       </Switch>
