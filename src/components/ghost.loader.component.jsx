4c1bd05a34697b9d377a87272f91630665cd4ee3
Erick Rodriguez <erickrod54@gmail.com>
2025-10-10 21:32:00 -0500

    Portfolio-erick - version 50.00 - App js file - Features: Implementing GhostWrapper

--- src/components/ghost.loader.component.jsx
+++ src/components/ghost.loader.component.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-/**Portfolio-erick - version 49.20 - App  js file -
+/**Portfolio-erick - version 50.00 - App  js file -
  * Features:
  * 
- *      --> Building 'GhostWrapper' 
+ *      --> Implementing 'GhostWrapper' 
  * 
  * Notes: To substite the legacy body implementation.
  */
@@ -331,44 +331,47 @@ const GhostWrapper = styled.div`
 const GhostLoader = () => {
 
     return(
+        <GhostWrapper>
+
         <div id="ghost">
-        <div id="red">
-            <div id="pupil"></div>
-            <div id="pupil1"></div>
-            <div id="eye"></div>
-            <div id="eye1"></div>
-            <div id="top0"></div>
-            <div id="top1"></div>
-            <div id="top2"></div>
-            <div id="top3"></div>
-            <div id="top4"></div>
-            <div id="st0"></div>
-            <div id="st1"></div>
-            <div id="st2"></div>
-            <div id="st3"></div>
-            <div id="st4"></div>
-            <div id="st5"></div>
-            <div id="an1"></div>
-            <div id="an2"></div>
-            <div id="an3"></div>
-            <div id="an4"></div>
-            <div id="an5"></div>
-            <div id="an6"></div>
-            <div id="an7"></div>
-            <div id="an8"></div>
-            <div id="an9"></div>
-            <div id="an10"></div>
-            <div id="an11"></div>
-            <div id="an12"></div>
-            <div id="an13"></div>
-            <div id="an14"></div>
-            <div id="an15"></div>
-            <div id="an16"></div>
-            <div id="an17"></div>
-            <div id="an18"></div>
-        </div>
-        <div id="shadow"></div>
+            <div id="red">
+                <div id="pupil"></div>
+                <div id="pupil1"></div>
+                <div id="eye"></div>
+                <div id="eye1"></div>
+                <div id="top0"></div>
+                <div id="top1"></div>
+                <div id="top2"></div>
+                <div id="top3"></div>
+                <div id="top4"></div>
+                <div id="st0"></div>
+                <div id="st1"></div>
+                <div id="st2"></div>
+                <div id="st3"></div>
+                <div id="st4"></div>
+                <div id="st5"></div>
+                <div id="an1"></div>
+                <div id="an2"></div>
+                <div id="an3"></div>
+                <div id="an4"></div>
+                <div id="an5"></div>
+                <div id="an6"></div>
+                <div id="an7"></div>
+                <div id="an8"></div>
+                <div id="an9"></div>
+                <div id="an10"></div>
+                <div id="an11"></div>
+                <div id="an12"></div>
+                <div id="an13"></div>
+                <div id="an14"></div>
+                <div id="an15"></div>
+                <div id="an16"></div>
+                <div id="an17"></div>
+                <div id="an18"></div>
+            </div>
+            <div id="shadow"></div>
         </div>   
+        </GhostWrapper>
     )
 }
 
