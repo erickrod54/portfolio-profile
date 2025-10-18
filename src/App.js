fb0d5d15695e8bee644861cdcb3b62928e939ed9
Erick Rodriguez <erickrod54@gmail.com>
2025-10-17 21:33:00 -0500

    Portfolio-erick - version 51.12 - App js file  - Features: Switching seventeenth_route to eighteenth_route

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { Redirect, Route, Switch, useLocation } from "wouter";
 import { useAuth0 } from "@auth0/auth0-react";
 import DashboardComponent from "./pages/dashboard.page.component.jsx";
 
-/**Portfolio-erick - version 51.11 - App  js file -
+/**Portfolio-erick - version 51.12 - App  js file -
  * Features:
  * 
- *      --> Placing 'seventeenth_route' in Route       
+ *      --> Switching 'seventeenth' to 'eighteenth_route'       
  * 
  * Notes: simulates the intial load 
  * ( will eventually refactored to handle fetching data)
@@ -153,7 +153,7 @@ const isAppLoading = isLoading || authLoading;
       style={{ filter:isLoading ? 'blur(1.5px)' : 'none', transition:'filter 0.3s'}}
     >
       <Switch>
-        <Route path={{seventeenth_route}}>
+        <Route path={eighteenth_route}>
             <Auth0CallbackHandler />
         </Route>
         <Route path={'/dashboard'}>
