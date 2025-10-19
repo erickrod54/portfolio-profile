2d86418fa804c95a002e16db073f61c467f1aa6c
Erick Rodriguez <erickrod54@gmail.com>
2025-10-18 21:30:00 -0500

    Portfolio-erick - version 51.15 - index js file - Features: Wrapping Auth0Provider

--- src/index.js
+++ src/index.js
@@ -4,28 +4,41 @@ import './index.css';
 import App from './App';
 import { PortfolioProvider } from './context';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-/**Portfolio-erick - version 16.02 - index js file -
+/**Portfolio-erick - version 51.15 - index js file -
  * Features:
  * 
- *      --> Adding Helmet for SEO and custom props
+ *      --> Wrapping with 'Auth0Provider'
  * 
  * Notes: Is pending to make a data structure to hold Helmet 
  * data and also develop a meta tag.
  */
 
+const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const  auth0ClientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Helmet>
-        <title>erickdev 👨🏾‍💻 {'{}'}</title>
-        <link rel='canonical' href='https://erickrod54.dev/'/>
-      </Helmet>
-      <PortfolioProvider>
-        < App />  
-      </PortfolioProvider>
-    </HelmetProvider>
+    <Auth0Provider
+      domain={auth0Domain}
+      clientId={auth0ClientId}
+      authorizationParams={{
+        redirect_uri:window.location.origin + '/callback',
+        scope:'openid profile email'
+      }}
+    >
+      <HelmetProvider>
+        <Helmet>
+          <title>erickdev 👨🏾‍💻 {'{}'}</title>
+          <link rel='canonical' href='https://erickrod54.dev/'/>
+        </Helmet>
+        <PortfolioProvider>
+          < App />  
+        </PortfolioProvider>
+      </HelmetProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
