97fd8b6fc99df6f5593b739e91c20b20a053c06f
Erick Rodriguez <erickrod54@gmail.com>
2025-10-20 21:35:00 -0500

    Portfolio-erick - version 52.09 - index js file - Features: Destructuring href_url

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { PortfolioProvider } from './context';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-/**Portfolio-erick - version 52.08 - index js file -
+/**Portfolio-erick - version 52.09 - index js file -
  * Features:
  * 
- *      --> Placing 'canonical' 
+ *      --> Destructuring 'href_url' 
  * 
  * Notes: Curley braces make 'eighteenth_route' to be 
  * treated as an object, and what it cause an authentication
@@ -34,7 +34,7 @@ const HelmetData = [
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const  auth0ClientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-const [{name:title}, {name:canonical}] = HelmetData;
+const [{name:title}, {name:canonical}, { name: href_url }] = HelmetData;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
