5b634ba87f983262bb8885caa1ca6207d18b8f21
Erick Rodriguez <erickrod54@gmail.com>
2025-10-24 21:33:00 -0500

    adding tailwind.config file to add sources where tailwind is going to be applied

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Ensure this path is correct for your React source files
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
