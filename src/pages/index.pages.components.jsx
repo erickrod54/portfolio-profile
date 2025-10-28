c6a2d8d89d5fbe1a1615ba7ee7ce8fb4e1a832c6
Erick Rodriguez <erickrod54@gmail.com>
2025-10-27 21:32:00 -0500

    Portfolio-erick - version 53.03 - HomePage - Features: Importing and exporting DashboardComponent

--- src/pages/index.pages.components.jsx
+++ src/pages/index.pages.components.jsx
@@ -1,13 +1,14 @@
+import DashboardComponent from "./dashboard.page.component.tsx";
 import HomePageComponent from "./home.page.component";
 import UpdatingPageComponent from "./work.in.progress.page.component";
 
-/**Portfolio-erick - version 46.06 - HomePage -
+/**Portfolio-erick - version 53.03 - HomePage -
  * Features:
  * 
- *      --> Importing and exporting 'UpdatingPageComponent'
+ *      --> Importing and exporting 'DashboardComponent'
  * 
  * Notes: This file is gonna be changed later.
 */
 
 
-export { HomePageComponent, UpdatingPageComponent }
+export { HomePageComponent, UpdatingPageComponent, DashboardComponent }
