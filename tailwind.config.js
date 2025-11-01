baae60ee003c29c7b4ae6c372d71b8097a46b922
Erick Rodriguez <erickrod54@gmail.com>
2025-10-31 21:33:00 -0500

    re creating tailwind config js file ( still conflicts with shadcn )

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // Ensure this path is correct for your React source files
-    "./src/**/*.{js,jsx,ts,tsx}", 
-    "./public/index.html",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: { extend: {}, },
   plugins: [],
 }
