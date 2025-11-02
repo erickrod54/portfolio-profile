dd9bf26b9629cdb2d25702c66942cb0c48c8a03d
Erick Rodriguez <erickrod54@gmail.com>
2025-11-01 21:31:00 -0500

    installing Vite tooling for as new Front-end for portfolio-profile, migrating first component and installing styled-components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import UpdatingPageComponent from './pages/work.in.progress.page.component'
+
+/**Portfolio-erick - version 53.04 - Apps -
+ * Features:
+ * 
+ *      --> Rendering 'UpdatingPageComponent'      
+ * 
+ * Notes: This 'UpdatingPageComponent' will render while
+ * the migration is being performed, this App js is from
+ * the new vite tooling - Having React + JS
+ */
+
+function App() {
+
+  return (
+    <>
+      <UpdatingPageComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/work.in.progress.page.component.jsx
@@ -0,0 +1,132 @@
+import styled, { css, keyframes } from "styled-components";
+//import { useLocation } from "wouter";
+
+/**Portfolio-erick - version 53.04 - UpdatingPageComponent -
+ * Features:
+ * 
+ *      --> First 'UpdatingPageComponent' migrated to Vite      
+ * 
+ * Notes: Vite will keep the front end of my portfolio,
+ * UpdatingPageComponent will render while migration  
+ */
+
+const backgroundAnimation = keyframes`
+  @keyframes {
+    0% {
+      background-position: 0%;
+    }
+    50% {
+      background-position: 400%;
+    }
+    100% {
+      background-position: 0%;
+    }
+`
+
+const FrontText = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0%;
+    background: var(--color-gar);
+    -webkit-background-clip: text;
+    background-clip: text;
+    background-size: 200%;
+    overflow: hidden;
+    transition: all 1s;
+    animation: ${backgroundAnimation} 8s infinite;
+    border-bottom: 2px solid transparent;
+
+    ${props =>
+      props.$isFilled &&
+      css`
+      width: 100%;
+      border-bottom: 2px solid #03a9f4;
+      -webkit-text-stroke: 1px var(--ani-color);
+      `}
+      
+  
+ `
+
+const ButtonTitle = styled.button`
+    position: relative;
+    border: none;
+    background: transparent;
+    --stroke-color: #ffffff7c;
+    --ani-color: rgba(95, 3, 244, 0);
+    --color-gar: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+    letter-spacing: 3px;
+    font-size: 2em;
+    font-family: "Arial";
+    text-transform: uppercase;
+    color: transparent;
+    -webkit-text-stroke: 1px var(--stroke-color);
+    cursor: pointer;
+  
+  
+    &:hover ${FrontText} {
+      width: 100%;
+      border-bottom: 2px solid #03a9f4;
+      -webkit-text-stroke: 1px var(--ani-color);
+    }
+`
+
+const WorkInProgressTitle = styled.h1`
+  color:white;
+`
+
+const AbsoluteButtonWrapper = styled.div`
+  position:absolute;
+  top:40px;
+  left:40px;
+  width:auto;
+`
+
+const StyledPath = styled(ButtonTitle)`
+  color:purple;
+  font-size:1.5rem;
+  margin-left:10px;
+`
+
+const UpdatingPageWrapper = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  
+  position: relative;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  flex-direction: column; 
+`
+
+const UpdatingData = [
+  {
+    message: 'Updating - 😉 work in progress'
+  },
+  {
+    message:'new portfolio version'
+  }
+]
+
+const UpdatingPageComponent = () => {
+
+  //const [ currentPath ] = useLocation();
+
+  const [ { message: updating }, { message: version } ] = UpdatingData;
+
+  return (
+    <UpdatingPageWrapper>
+      <WorkInProgressTitle>
+          {updating} on 
+        <StyledPath>
+          {version}
+        </StyledPath>
+        </WorkInProgressTitle> 
+    </UpdatingPageWrapper>
+      
+  );
+}
+
+export default UpdatingPageComponent;
+
