66ff6b5172b11de1694289fbc8332db481e7f73c
Erick Rodriguez <erickrod54@gmail.com>
2025-11-01 21:33:00 -0500

    Portfolio-erick - version 53.05 - index.pages - Features: Importing and exporting UpdatingPageComponent

---
+++ src/pages/index.page.components.jsx
@@ -0,0 +1,11 @@
+import UpdatingPageComponent from "./work.in.progress.page.component";
+
+/**Portfolio-erick - version 53.05 - index.pages  -
+ * Features:
+ * 
+ *      --> Importing and exporting 'UpdatingPageComponent'
+ * 
+ * Notes: Next component to migrate will be HomePageComponent.
+*/
+
+export { UpdatingPageComponent }
