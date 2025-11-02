f3072c0ce24b34dd081cd200f4492e19a9691cc4
Erick Rodriguez <erickrod54@gmail.com>
2025-11-01 21:35:00 -0500

    Portfolio-erick - version 53.06 - index > components - Features: Migrating HeaderTitle Vite version

---
+++ src/components/index.components.js
@@ -0,0 +1,12 @@
+import HeaderTitle from "./header.title.component";
+
+/**Portfolio-erick - version 53.06 - index > components
+ *  - Features:
+ * 
+ *      --> Migrationg 'HeaderTitle' Vite version.
+ * 
+ * Notes: This component now is on legacy directory
+ */
+
+export { HeaderTitle }
+
