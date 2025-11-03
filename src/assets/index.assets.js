49d259efe8094c5e88c5e5c9d4962acd888bb979
Erick Rodriguez <erickrod54@gmail.com>
2025-11-02 21:35:00 -0600

    Portfolio-erick - version 53.08 - assets > index - Features: Importing and exporting profilePicture

---
+++ src/assets/index.assets.js
@@ -0,0 +1,11 @@
+import profilePicture from './Profile_picture_2.jpg'
+
+/**Portfolio-erick - version 53.08 - assets > index -
+* Features:
+
+    -→> Importing and exporting 'profilePicture'
+
+* Notes: importing and exporting profilePicture
+**/
+
+export { profilePicture }
