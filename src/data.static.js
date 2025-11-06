3e4e14b50b4f2a6d6db0007beb64267a7b11a6d4
Erick Rodriguez <erickrod54@gmail.com>
2025-11-05 21:35:00 -0600

    Portfolio-erick - version 53.14 - data js - Features: Adding profilePictureImg

--- src/data.static.js
+++ src/data.static.js
@@ -1,10 +1,14 @@
-/**Portfolio-erick - version 53.13 - data js
+import { profilePicture } from "./assets/index.assets";
+
+/**Portfolio-erick - version 53.14 - data js
  *  - Features:
  * 
- *      --> Adding 'data_test'.
+ *      --> Adding 'profilePictureImg'.
  * 
  * Notes: This is the first test for context static 
  * data
  */
+
+export const profilePictureImg = profilePicture;
 
 export const data_test = 'testing data context';
