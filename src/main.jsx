d6ee5cad0ed49e2c895c2625ef88c4873301b200
Erick Rodriguez <erickrod54@gmail.com>
2025-11-05 21:34:00 -0600

    Portfolio-erick - version 53.13 - main jsx - Features: Wrapping with DataContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { DataContextProvider } from './context.data.jsx'
+       
+/**Portfolio-erick - version 53.13 - main jsx
+ *  - Features:
+ *  
+ *      --> Wrapping  with 'DataContextProvider'.
+ * 
+ * Notes: This provider soon will be replaced with
+ * the global provider
+ */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
   </StrictMode>,
 )
