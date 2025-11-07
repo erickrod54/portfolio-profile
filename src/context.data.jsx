35d0f813a6340ae5c5ff84b9a83665e9b45dc3b4
Erick Rodriguez <erickrod54@gmail.com>
2025-11-06 21:30:00 -0600

    Portfolio-erick - version 53.14 - context.data - Features: Importing and providing profilePictureImg

--- src/context.data.jsx
+++ src/context.data.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import { data_test } from "./data.static";
+import { data_test, profilePictureImg } from "./data.static";
 
-/**Portfolio-erick - version 53.13 - context.data
+/**Portfolio-erick - version 53.14 - context.data
  *  - Features:
  *  
- *      --> Building DataContext.
+ *      --> Importing and providing 'profilePictureImg'.
  * 
  * Notes: This context will provide static data for 
  * the components that need it 
@@ -17,7 +17,8 @@ export const DataContextProvider = ({ children }) => {
     return(
         <DataContext.Provider 
             value={{
-                data_test
+                data_test,
+                profilePictureImg
                 }}>
             {children}
         </DataContext.Provider>
