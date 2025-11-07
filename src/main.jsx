f54530bc5e46560f4a4ba43c577e47e1f9f310cb
Erick Rodriguez <erickrod54@gmail.com>
2025-11-06 21:35:00 -0600

    Portfolio-erick - version 53.15 - main js - Features: Wrapping with IconsContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { DataContextProvider } from './context.data.jsx'
+import { IconsContextProvider } from './context.icons.data.jsx'
        
-/**Portfolio-erick - version 53.13 - main jsx
+/**Portfolio-erick - version 53.15 - main js
  *  - Features:
  *  
- *      --> Wrapping  with 'DataContextProvider'.
+ *      --> Wrapping  with 'IconsContextProvider'.
  * 
  * Notes: This provider soon will be replaced with
  * the global provider
@@ -15,8 +16,10 @@ import { DataContextProvider } from './context.data.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DataContextProvider>
-      <App />
-    </DataContextProvider>
+    <IconsContextProvider>
+      <DataContextProvider>
+        <App />
+      </DataContextProvider>
+    </IconsContextProvider>
   </StrictMode>,
 )
