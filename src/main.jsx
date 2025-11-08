80f48c0871b6161a8616f0a72d582ad44ea6185f
Erick Rodriguez <erickrod54@gmail.com>
2025-11-07 21:35:00 -0600

    Portfolio-erick - version 53.17 - main js  - Features: Changing imports directory

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { DataContextProvider } from './context.data.jsx'
-import { IconsContextProvider } from './context.icons.data.jsx'
+import { DataContextProvider } from './contexts/context.data.jsx'
+import { IconsContextProvider } from './contexts/context.icons.data.jsx'
        
-/**Portfolio-erick - version 53.15 - main js
+/**Portfolio-erick - version 53.17 - main js
  *  - Features:
  *  
- *      --> Wrapping  with 'IconsContextProvider'.
+ *      --> Changing imports directory.
  * 
  * Notes: This provider soon will be replaced with
  * the global provider
