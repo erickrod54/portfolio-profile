c555a1258321216062b51119659124441af13af0
Erick Rodriguez <erickrod54@gmail.com>
2025-11-08 21:30:00 -0600

    Portfolio-erick - version 53.17 - data js - Fetaures: Adding social

--- src/data.static.js
+++ src/data.static.js
@@ -1,13 +1,41 @@
 import { profilePicture } from "./assets/index.assets";
 
-/**Portfolio-erick - version 53.14 - data js
+/**Portfolio-erick - version 53.17 - data js
  *  - Features:
  * 
- *      --> Adding 'profilePictureImg'.
+ *      --> Adding 'social'.
  * 
  * Notes: This is the first test for context static 
  * data
  */
+
+export const social = [
+    {
+        name:'Github',
+        icon: 'githubIcon',
+        url:'https://github.com/erickrod54'
+    },
+    {
+        name:'Linkdin',
+        icon: 'linkedlnIcon',
+        url:'https://www.linkedin.com/in/erickrodriguezcardenas'
+    },
+    {
+        name:'X',
+        icon:'',
+        url:'https://x.com/erickdev54?s=21'
+    },
+    {
+        name:'Twitch',
+        icon: '',
+        url:'https://dashboard.twitch.tv/u/erickdev54/'
+    },
+    {
+        name:'Discord',
+        icon: '',
+        url:'https://discord.gg/t8Ka7me4en'
+    },
+]
 
 export const profilePictureImg = profilePicture;
 
