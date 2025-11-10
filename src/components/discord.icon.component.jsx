376e5c8e6d73d4d0aab2ebc01b8f7c25667341da
Erick Rodriguez <erickrod54@gmail.com>
2025-11-09 21:31:00 -0600

    Portfolio-erick - version 53.19 - SocialComponent - Features: Clearing local social data

--- src/components/discord.icon.component.jsx
+++ src/components/discord.icon.component.jsx
@@ -2,42 +2,14 @@ import { useDataContext } from '../contexts/context.data';
 import { useIconsContext } from '../contexts/context.icons.data';
 import IconComponent from './icon.component';
 
-/**Portfolio-erick - version 53.18 - SocialComponent -
+/**Portfolio-erick - version 53.19 - SocialComponent -
 * Features:
 
-    --> Destructuring 'social' from context
+    --> Clearing local 'social' data
 
 * Notes: className has .svg class for this component
 *later will be refacotored
 **/
-
-const social = [
-    {
-        name:'Github',
-        icon: 'githubIcon',
-        url:'https://github.com/erickrod54'
-    },
-    {
-        name:'Linkdin',
-        icon: 'linkedlnIcon',
-        url:'https://www.linkedin.com/in/erickrodriguezcardenas'
-    },
-    {
-        name:'X',
-        icon:'',
-        url:'https://x.com/erickdev54?s=21'
-    },
-    {
-        name:'Twitch',
-        icon: '',
-        url:'https://dashboard.twitch.tv/u/erickdev54/'
-    },
-    {
-        name:'Discord',
-        icon: '',
-        url:'https://discord.gg/t8Ka7me4en'
-    },
-]
 
 const DiscordIcon = ({ className }) => {
 
