43d461344a523cb58e3815bb8d2b9ec4a3ed45c3
Erick Rodriguez <erickrod54@gmail.com>
2025-11-13 21:34:00 -0600

    Portfolio-erick - version 54.09 - index > components - Features: Migrating FloatCard Vite version

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,13 +1,14 @@
 import Card from "./card.component";
+import FloatCard from "./float.card.component";
 import HeaderTitle from "./header.title.component";
 
-/**Portfolio-erick - version 54.08 - index > components
+/**Portfolio-erick - version 54.09 - index > components
  *  - Features:
  * 
- *      --> Migrationg 'Card' Vite version.
+ *      --> Migrating 'FloatCard' Vite version.
  * 
  * Notes: This component now is on legacy directory
  */
 
-export { HeaderTitle, Card }
+export { HeaderTitle, Card, FloatCard }
 
