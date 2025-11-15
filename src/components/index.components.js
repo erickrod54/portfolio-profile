b1d24678f030b768830b1c8265b9d7acbdf3cc40
Erick Rodriguez <erickrod54@gmail.com>
2025-11-14 21:31:00 -0600

    Portfolio-erick - version 54.10 - index > components - Features: Migrating sectionDivider Vite version

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,14 +1,15 @@
 import Card from "./card.component";
 import FloatCard from "./float.card.component";
 import HeaderTitle from "./header.title.component";
+import SectionDivider from "./section.divider.component";
 
-/**Portfolio-erick - version 54.09 - index > components
+/**Portfolio-erick - version 54.10 - index > components
  *  - Features:
  * 
- *      --> Migrating 'FloatCard' Vite version.
+ *      --> Migrating 'SectionDivider' Vite version.
  * 
  * Notes: This component now is on legacy directory
  */
 
-export { HeaderTitle, Card, FloatCard }
+export { HeaderTitle, Card, FloatCard, SectionDivider }
 
