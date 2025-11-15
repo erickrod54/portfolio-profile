29cf6530dbc835f5f78d653c4176964583de3071
Erick Rodriguez <erickrod54@gmail.com>
2025-11-14 21:35:00 -0600

    Portfolio-erick - version 54.11 - index > components - Features: Migrating WaterTitle to Vite version

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -2,14 +2,15 @@ import Card from "./card.component";
 import FloatCard from "./float.card.component";
 import HeaderTitle from "./header.title.component";
 import SectionDivider from "./section.divider.component";
+import WaterTitle from "./water.title.component";
 
-/**Portfolio-erick - version 54.10 - index > components
+/**Portfolio-erick - version 54.11 - index > components
  *  - Features:
  * 
- *      --> Migrating 'SectionDivider' Vite version.
+ *      --> Migrating 'WaterTitle' Vite version.
  * 
  * Notes: This component now is on legacy directory
  */
 
-export { HeaderTitle, Card, FloatCard, SectionDivider }
+export { HeaderTitle, Card, FloatCard, SectionDivider, WaterTitle }
 
