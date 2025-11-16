a18f358b3dc6ee0d8615a280f2a8fbd19f8601b9
Erick Rodriguez <erickrod54@gmail.com>
2025-11-15 21:34:00 -0600

    Portfolio-erick - version 54.14 - index > components - Features: Migrating CollabTitle Vite version

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,18 +1,23 @@
 import Card from "./card.component";
 import Collaborations from "./collaborations.component";
+import CollabTitle from "./collaborations.title.component";
 import FloatCard from "./float.card.component";
 import HeaderTitle from "./header.title.component";
 import SectionDivider from "./section.divider.component";
 import WaterTitle from "./water.title.component";
 import WizardCollabTitle from "./wizard.collab.title.component";
 
-/**Portfolio-erick - version 54.12 - index > components
+/**Portfolio-erick - version 54.14 - index > components
  *  - Features:
  * 
- *      --> Migrating 'Collaborations' Vite version.
+ *      --> Migrating 'CollabTitle' Vite version.
  * 
  * Notes: This component now is on legacy directory
  */
 
-export { HeaderTitle, Card, FloatCard, SectionDivider, WaterTitle, WizardCollabTitle, Collaborations }
+export { HeaderTitle,
+         Card,
+         FloatCard,
+         SectionDivider,
+         WaterTitle, WizardCollabTitle, Collaborations, CollabTitle }
 
