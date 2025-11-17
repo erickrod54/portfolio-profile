b33ca9d726a969edbaf3c6ca4b6ba0934cd35105
Erick Rodriguez <erickrod54@gmail.com>
2025-11-16 21:35:00 -0600

    Portfolio-erick - version 54.17 - index > components - Features: Migrating IconComponent Vite version

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -4,14 +4,15 @@ import CollabTitle from "./collaborations.title.component";
 import FloatCard from "./float.card.component";
 import GitChart from "./git.chart.component";
 import HeaderTitle from "./header.title.component";
+import IconComponent from "./icon.component";
 import SectionDivider from "./section.divider.component";
 import WaterTitle from "./water.title.component";
 import WizardCollabTitle from "./wizard.collab.title.component";
 
-/**Portfolio-erick - version 54.15 - index > components
+/**Portfolio-erick - version 54.17 - index > components
  *  - Features:
  * 
- *      --> Migrating 'GitChart' Vite version.
+ *      --> Migrating 'IconComponent' Vite version.
  * 
  * Notes: This component now is on legacy directory
  */
@@ -21,5 +22,6 @@ export { HeaderTitle,
          FloatCard,
          SectionDivider,
          GitChart,
+         IconComponent,
          WaterTitle, WizardCollabTitle, Collaborations, CollabTitle }
 
