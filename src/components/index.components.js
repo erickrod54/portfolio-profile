d9449509e11eae8a9e1050160e343362bc8bffa2
Erick Rodriguez <erickrod54@gmail.com>
2025-11-16 21:31:00 -0600

    Portfolio-erick - version 54.15 - index > components - Features: Migrating GitChart Vite version

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -2,15 +2,16 @@ import Card from "./card.component";
 import Collaborations from "./collaborations.component";
 import CollabTitle from "./collaborations.title.component";
 import FloatCard from "./float.card.component";
+import GitChart from "./git.chart.component";
 import HeaderTitle from "./header.title.component";
 import SectionDivider from "./section.divider.component";
 import WaterTitle from "./water.title.component";
 import WizardCollabTitle from "./wizard.collab.title.component";
 
-/**Portfolio-erick - version 54.14 - index > components
+/**Portfolio-erick - version 54.15 - index > components
  *  - Features:
  * 
- *      --> Migrating 'CollabTitle' Vite version.
+ *      --> Migrating 'GitChart' Vite version.
  * 
  * Notes: This component now is on legacy directory
  */
@@ -19,5 +20,6 @@ export { HeaderTitle,
          Card,
          FloatCard,
          SectionDivider,
+         GitChart,
          WaterTitle, WizardCollabTitle, Collaborations, CollabTitle }
 
