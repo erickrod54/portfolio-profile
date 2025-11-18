94a373a8c9c9fda61413fa6457d595a43ea3dcaa
Erick Rodriguez <erickrod54@gmail.com>
2025-11-17 21:33:00 -0600

    Portfolio-erick - version 54.01 - utils > index - Features: Importing and exporting getCurrentYear

---
+++ src/utils/utils.index.js
@@ -0,0 +1,13 @@
+import { getCurrentYear } from "./utils";
+
+/**Portfolio-erick - version 54.01  - utils > index -
+ * Features:
+ * 
+ *      --> Importing and exporting 'getCurrentYear' 
+ * 
+ * Notes: This components is adapted to the new layout
+ * and is functional working, pending for layout the 
+ * component itself
+ */
+
+export { getCurrentYear }
