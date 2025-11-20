a2c4acee45c7c2271d1fedf9f8e9310d8d357ff5
Erick Rodriguez <erickrod54@gmail.com>
2025-11-19 21:35:00 -0600

    Portfolio-erick - version 55.00 - main js - Features: Implementing Helment on root directory Vite

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,44 @@ import './index.css'
 import App from './App.jsx'
 import { DataContextProvider } from './contexts/context.data.jsx'
 import { IconsContextProvider } from './contexts/context.icons.data.jsx'
-       
-/**Portfolio-erick - version 53.17 - main js
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+   
+/**Portfolio-erick - version 55.00 - main js
  *  - Features:
  *  
- *      --> Changing imports directory.
+ *      --> Implementing 'Helmet' on root directory
  * 
  * Notes: This provider soon will be replaced with
  * the global provider
  */
 
+const HelmetData = [
+  {
+    name:'erickdev',
+    placement:'title'
+  },{
+    name:'canonical',
+    placement:'rel'
+  },{
+    name:'https://erickrod54.dev/',
+    placement:'href'
+  }
+]
+
+const [{ name: title }, { name: canonical }, { name: href_url }] = HelmetData;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <IconsContextProvider>
-      <DataContextProvider>
-        <App />
-      </DataContextProvider>
-    </IconsContextProvider>
+    <HelmetProvider>
+      <Helmet>
+        <title>{title} 👨🏾‍💻 {'{}'}</title>
+        <link rel={canonical} href={href_url}/>
+      </Helmet>
+      <IconsContextProvider>
+        <DataContextProvider>
+          <App />
+        </DataContextProvider>
+      </IconsContextProvider>
+    </HelmetProvider>
   </StrictMode>,
 )
