3a1e1097ffea0df44b9e42fc7bac3019f8e85e68
Erick Rodriguez <erickrod54@gmail.com>
2025-11-20 21:30:00 -0600

    installing npm install -D @vitejs/plugin-basic-ssl --legacy-peer-deps to run local on https for auth0

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()
+  ],
   server: {
     host: '0.0.0.0',
-    port: 3000,     
+    port: 3000,
+    https:true
   },
 });
