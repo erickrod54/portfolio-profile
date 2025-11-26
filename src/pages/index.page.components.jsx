0f5d0e95f8c6fa0de9438e69099164f3f0f444c9
Erick Rodriguez <erickrod54@gmail.com>
2025-11-25 21:32:00 -0600

    Portfolio-erick - version 55.10 - index.pages - Features: Importing and exporting AboutPage

--- src/pages/index.page.components.jsx
+++ src/pages/index.page.components.jsx
@@ -2,13 +2,14 @@ import UpdatingPageComponent from "./work.in.progress.page.component";
 import HomePageComponent from './home.page.component';
 import DashboardComponent from './dashboard.page.component.jsx';
 import OverviewPage from "./overview.page.component.jsx";
+import AboutPage from "./about.page.component.jsx";
 
-/**Portfolio-erick - version 55.09 - index.pages  -
+/**Portfolio-erick - version 55.10 - index.pages  -
  * Features:
  * 
- *      --> Importing and exporting 'OverviewPage'
+ *      --> Importing and exporting 'AboutPage'
  * 
  * Notes: subcomponents of Home will be checked.
 */
 
-export { DashboardComponent, UpdatingPageComponent, HomePageComponent, OverviewPage }
+export { DashboardComponent, UpdatingPageComponent, HomePageComponent, OverviewPage, AboutPage }
