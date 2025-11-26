d1cb6c43c8fd218bf06f06c46a10a9a0cd69c715
Erick Rodriguez <erickrod54@gmail.com>
2025-11-25 21:34:00 -0600

    Portfolio-erick - version 55.09 - data js - Features: Adding DummyFeaturesData

--- src/data.static.js
+++ src/data.static.js
@@ -1,9 +1,9 @@
 import { profilePicture } from "./assets/index.assets";
 
-/**Portfolio-erick - version 54.08 - data js
+/**Portfolio-erick - version 55.09 - data js
  *  - Features:
  * 
- *      --> Adding 'social_title'.
+ *      --> Adding 'DummyFeaturesData'.
  * 
  * Notes: This is the first test for context static 
  * data
@@ -50,3 +50,27 @@ export const social = [
 export const profilePictureImg = profilePicture;
 
 export const data_test = 'testing data context';
+
+export const DummyFeaturesData = [
+  {
+    subtitle: 'GET STARTED',
+    title: 'What level of service are you?',
+    text: 'Determining what level of service you need is an important tool when planning future projects. This guide will help you plan your next steps...',
+    linkText: 'Read more',
+    imageUrl: 'https://via.placeholder.com/450x400/0A0A0A/ccc?text=Idea+1+Image'
+  },
+  {
+    subtitle: 'CORE ESSENTIALS',
+    title: 'Picking the right Tech Stack!',
+    text: 'The nice thing about beginning a new project is that you don\'t really need any heavy frameworks. Start with small, core libraries and grow from there.',
+    linkText: 'Learn more',
+    imageUrl: 'https://via.placeholder.com/450x400/0A0A0A/ccc?text=Idea+2+Image'
+  },
+  {
+    subtitle: 'WHERE THE CODE LIVES',
+    title: 'Understand Your Hosting & Deployment',
+    text: 'To start, secure your domain, set up your CI/CD pipeline, and choose a reliable host. A solid foundation is key to performance.',
+    linkText: 'See options',
+    imageUrl: 'https://via.placeholder.com/450x400/0A0A0A/ccc?text=Idea+3+Image'
+  }
+];
