36fcb22f7981ed0fa32a7af5cfe3aca6e55c34a4
Erick Rodriguez <erickrod54@gmail.com>
2025-11-27 21:33:00 -0600

    Portfolio-erick - version 54.12 - context.data - Features: Importing and providing OverviewData

--- src/contexts/context.data.jsx
+++ src/contexts/context.data.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
-import { data_test, HeaderTitleData, profilePictureImg, social, social_title, DummyFeaturesData } from "../data.static";
+import { data_test,
+         HeaderTitleData,
+         profilePictureImg,
+         OverviewData,
+         social, social_title, DummyFeaturesData } from "../data.static";
 
-/**Portfolio-erick - version 54.10 - context.data
+/**Portfolio-erick - version 54.12 - context.data
  *  - Features:
  *  
- *      --> Importing and providing 'DummyFeaturesData'.
+ *      --> Importing and providing 'OverviewData'.
  * 
  * Notes: This context will provide static data for 
  * the components that need it 
@@ -23,6 +27,7 @@ export const DataContextProvider = ({ children }) => {
                 social_title,
                 HeaderTitleData,
                 DummyFeaturesData,
+                OverviewData,
                 }}>
             {children}
         </DataContext.Provider>
