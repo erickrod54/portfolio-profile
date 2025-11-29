f34f83f1b79aca10281b6b29f843af164356c4ab
Erick Rodriguez <erickrod54@gmail.com>
2025-11-28 21:31:00 -0600

    Portfolio-erick - version 53.13 - assets > index - Features: Importing and exporting blackSmith

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,11 +1,12 @@
-import profilePicture from './Profile_picture_2.jpg'
+import profilePicture from './Profile_picture_2.jpg';
+import blackSmith from './Magical_Assets/Blacksmith.jpeg';
 
-/**Portfolio-erick - version 53.08 - assets > index -
+/**Portfolio-erick - version 53.13 - assets > index -
 * Features:
 
-    -→> Importing and exporting 'profilePicture'
+    -→> Importing and exporting 'blackSmith'
 
 * Notes: importing and exporting profilePicture
 **/
 
-export { profilePicture }
+export { profilePicture, blackSmith }
