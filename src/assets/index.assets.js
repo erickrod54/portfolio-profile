f46b1bba1ec534af267d5362df49eeac2c50a772
Erick Rodriguez <erickrod54@gmail.com>
2025-11-28 21:34:00 -0600

    Portfolio-erick - version 53.15 - assets > index - Features: Importing and exporting roadMapPicture

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,12 +1,13 @@
 import profilePicture from './Profile_picture_2.jpg';
 import blackSmith from './Magical_Assets/Blacksmith.jpeg';
+import roadMapPicture from './Magical_Assets/Road_map.jpeg'
 
-/**Portfolio-erick - version 53.13 - assets > index -
+/**Portfolio-erick - version 53.15 - assets > index -
 * Features:
 
-    -→> Importing and exporting 'blackSmith'
+    -→> Importing and exporting 'roadMapPicture'
 
 * Notes: importing and exporting profilePicture
 **/
 
-export { profilePicture, blackSmith }
+export { profilePicture, blackSmith, roadMapPicture }
