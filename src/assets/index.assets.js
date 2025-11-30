9d56762c9def4a4521c77e2e6b8619af722b0193
Erick Rodriguez <erickrod54@gmail.com>
2025-11-29 21:31:00 -0600

    Portfolio-erick - version 53.16 - assets > index - Features: Importing and exporting questPicture

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,13 +1,14 @@
 import profilePicture from './Profile_picture_2.jpg';
 import blackSmith from './Magical_Assets/Blacksmith.jpeg';
 import roadMapPicture from './Magical_Assets/Road_map.jpeg'
+import questPicture from './Magical_Assets/Quest_mission.jpeg'
 
-/**Portfolio-erick - version 53.15 - assets > index -
+/**Portfolio-erick - version 53.16 - assets > index -
 * Features:
 
-    -→> Importing and exporting 'roadMapPicture'
+    -→> Importing and exporting 'QuestPicture'
 
 * Notes: importing and exporting profilePicture
 **/
 
-export { profilePicture, blackSmith, roadMapPicture }
+export { profilePicture, blackSmith, roadMapPicture, questPicture }
