bf623d89cbcc80cc897aac7e9943e2e062c53eb8
Erick Rodriguez <erickrod54@gmail.com>
2025-11-29 21:34:00 -0600

    Portfolio-erick - version 55.17 - assets > index - Features: Importing and exporting warriorResolve

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -2,13 +2,14 @@ import profilePicture from './Profile_picture_2.jpg';
 import blackSmith from './Magical_Assets/Blacksmith.jpeg';
 import roadMapPicture from './Magical_Assets/Road_map.jpeg'
 import questPicture from './Magical_Assets/Quest_mission.jpeg'
+import warriorResolve from './Magical_Assets/Warrior.jpeg'
 
-/**Portfolio-erick - version 53.16 - assets > index -
+/**Portfolio-erick - version 55.17 - assets > index -
 * Features:
 
-    -→> Importing and exporting 'QuestPicture'
+    -→> Importing and exporting 'warriorResolve'
 
 * Notes: importing and exporting profilePicture
 **/
 
-export { profilePicture, blackSmith, roadMapPicture, questPicture }
+export { profilePicture, blackSmith, roadMapPicture, questPicture, warriorResolve }
