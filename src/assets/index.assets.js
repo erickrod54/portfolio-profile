bdfe38528dfbf41d0a37e359bc467459328884be
Erick Rodriguez <erickrod54@gmail.com>
2025-11-30 21:31:00 -0600

    Portfolio-erick - version 55.18 - assets > index - Features: Importing and exporting priestInsight

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -3,13 +3,14 @@ import blackSmith from './Magical_Assets/Blacksmith.jpeg';
 import roadMapPicture from './Magical_Assets/Road_map.jpeg'
 import questPicture from './Magical_Assets/Quest_mission.jpeg'
 import warriorResolve from './Magical_Assets/Warrior.jpeg'
+import priestInsight from './Magical_Assets/Holy_priest.jpeg'
 
-/**Portfolio-erick - version 55.17 - assets > index -
+/**Portfolio-erick - version 55.18 - assets > index -
 * Features:
 
-    -→> Importing and exporting 'warriorResolve'
+    -→> Importing and exporting 'priestInsight'
 
 * Notes: importing and exporting profilePicture
 **/
 
-export { profilePicture, blackSmith, roadMapPicture, questPicture, warriorResolve }
+export { profilePicture, blackSmith, roadMapPicture, questPicture, warriorResolve, priestInsight }
