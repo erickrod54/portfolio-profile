e9fef9f82ce65ef02fe4bce42fb3e7663f21fbdc
Erick Rodriguez <erickrod54@gmail.com>
2025-11-30 21:34:00 -0600

    Portfolio-erick - version 55.19 - assets > index - Features: Importing and exporting druidBalance

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -4,13 +4,14 @@ import roadMapPicture from './Magical_Assets/Road_map.jpeg'
 import questPicture from './Magical_Assets/Quest_mission.jpeg'
 import warriorResolve from './Magical_Assets/Warrior.jpeg'
 import priestInsight from './Magical_Assets/Holy_priest.jpeg'
+import druidBalance from './Magical_Assets/Druid.jpeg';
 
-/**Portfolio-erick - version 55.18 - assets > index -
+/**Portfolio-erick - version 55.19 - assets > index -
 * Features:
 
-    -→> Importing and exporting 'priestInsight'
+    -→> Importing and exporting 'druidBalance'
 
 * Notes: importing and exporting profilePicture
 **/
 
-export { profilePicture, blackSmith, roadMapPicture, questPicture, warriorResolve, priestInsight }
+export { profilePicture, blackSmith, roadMapPicture, questPicture, warriorResolve, priestInsight, druidBalance }
