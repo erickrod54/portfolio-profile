78dcd91f1a645edefbbfa45e913346fc216fabb5
Erick Rodriguez <erickrod54@gmail.com>
2025-12-02 21:35:00 -0600

    Portfolio-erick - version 56.03 - index.pages - Features: Clearing TeamMemberCard

--- src/pages/index.page.components.jsx
+++ src/pages/index.page.components.jsx
@@ -4,13 +4,12 @@ import DashboardComponent from './dashboard.page.component.jsx';
 import OverviewPage from "./overview.page.component.jsx";
 import AboutPage from "./about.page.component.jsx";
 import FooterAllPages from "./footer.all.pages.jsx";
-import TeamMemberCard from "../components/team.member.card.component.jsx";
 import TeamPage from "./team.page.component.jsx";
 
-/**Portfolio-erick - version 56.02 - index.pages  -
+/**Portfolio-erick - version 56.03 - index.pages  -
  * Features:
  * 
- *      --> Importing and exporting 'TeamGrid'
+ *      --> Clearing 'TeamMemberCard'
  * 
  * Notes: subcomponents of Home will be checked.
 */
@@ -19,4 +18,4 @@ export { DashboardComponent,
          UpdatingPageComponent,
          HomePageComponent,
          TeamPage,
-         OverviewPage, AboutPage, FooterAllPages, TeamMemberCard }
+         OverviewPage, AboutPage, FooterAllPages }
