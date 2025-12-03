e9fcfc859cf8f358646a66b989faf501c6181a3e
Erick Rodriguez <erickrod54@gmail.com>
2025-12-02 21:31:00 -0600

    Portfolio-erick - version 56.01 - index.pages - Features: Importing and exporting TeamMemberCard

--- src/pages/index.page.components.jsx
+++ src/pages/index.page.components.jsx
@@ -4,13 +4,17 @@ import DashboardComponent from './dashboard.page.component.jsx';
 import OverviewPage from "./overview.page.component.jsx";
 import AboutPage from "./about.page.component.jsx";
 import FooterAllPages from "./footer.all.pages.jsx";
+import TeamMemberCard from "../components/team.member.card.component.jsx";
 
-/**Portfolio-erick - version 56.00 - index.pages  -
+/**Portfolio-erick - version 56.01 - index.pages  -
  * Features:
  * 
- *      --> Importing and exporting 'FooterAllPages'
+ *      --> Importing and exporting 'TeamMemberCard'
  * 
  * Notes: subcomponents of Home will be checked.
 */
 
-export { DashboardComponent, UpdatingPageComponent, HomePageComponent, OverviewPage, AboutPage, FooterAllPages }
+export { DashboardComponent,
+         UpdatingPageComponent,
+         HomePageComponent,
+         OverviewPage, AboutPage, FooterAllPages, TeamMemberCard }
