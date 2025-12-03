cde56e3758cbd92bf591318f08b43cfa64f6e776
Erick Rodriguez <erickrod54@gmail.com>
2025-12-02 21:30:00 -0600

    Portfolio-erick - version 56.00 - index.pages - Features: Importing and exporting FooterAllPages

--- src/pages/index.page.components.jsx
+++ src/pages/index.page.components.jsx
@@ -3,13 +3,14 @@ import HomePageComponent from './home.page.component';
 import DashboardComponent from './dashboard.page.component.jsx';
 import OverviewPage from "./overview.page.component.jsx";
 import AboutPage from "./about.page.component.jsx";
+import FooterAllPages from "./footer.all.pages.jsx";
 
-/**Portfolio-erick - version 55.10 - index.pages  -
+/**Portfolio-erick - version 56.00 - index.pages  -
  * Features:
  * 
- *      --> Importing and exporting 'AboutPage'
+ *      --> Importing and exporting 'FooterAllPages'
  * 
  * Notes: subcomponents of Home will be checked.
 */
 
-export { DashboardComponent, UpdatingPageComponent, HomePageComponent, OverviewPage, AboutPage }
+export { DashboardComponent, UpdatingPageComponent, HomePageComponent, OverviewPage, AboutPage, FooterAllPages }
