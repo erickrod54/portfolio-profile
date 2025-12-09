8591f1ae7a9998f47388057fcdaea213fadc699d
Erick Rodriguez <erickrod54@gmail.com>
2025-12-08 21:34:00 -0600

    adding tailwind to vite conf file (by official tailwind docs)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     basicSsl()
   ],
