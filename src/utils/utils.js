bf6191f593218f098731d1cedeb35ed67cfd6908
Erick Rodriguez <erickrod54@gmail.com>
2025-12-14 21:35:00 -0600

    Portfolio-erick - version 56.10 - utils - Features: Migrating cn to merge tailwind styles

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,25 @@
-/**Portfolio-erick - version 54.19 - utils - Features:
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**Portfolio-erick - version 56.10 - utils - Features:
  * 
- *      --> Migrating 'getCurrentYear' to Vite version
+ *      --> Migrating 'cn' to merge tailwind styles
  * 
- * Notes: This component receives 'children' components
+ * Notes: cn use to:
+ * 
+ *  1.  Removes false / null / undefined automatically
+ *	2.	Resolves Tailwind conflicts
+ *	3.	Works perfectly with CVA variants
+ *	4.	Keeps class logic declarative
+ *
  */
 
 /**javascript util that get the full current year */
 export const getCurrentYear = () => {
     return new Date().getFullYear();
+}
+
+// standard cn util
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
 }
