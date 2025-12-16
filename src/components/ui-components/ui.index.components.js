6b94d9365315b8c7a433b32b865f18635bef5a54
Erick Rodriguez <erickrod54@gmail.com>
2025-12-15 21:31:00 -0600

    Portfolio-erick - version 56.10 - UI components > index - Features: Importing and exporting loadingVariants

---
+++ src/components/ui-components/ui.index.components.js
@@ -0,0 +1,12 @@
+import { loadingVariants } from "./loading.messages.component";
+
+/**Portfolio-erick - version 56.10 - UI components > index -
+* Features:
+
+    --> Importing and exporting 'loadingVariants'
+
+* Notes: className has .svg class for this component
+*later will be refacotored
+**/
+
+export { loadingVariants }
