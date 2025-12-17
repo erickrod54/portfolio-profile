cd93edb73e5216c1e6cb98e26f2ecfd333e24141
Erick Rodriguez <erickrod54@gmail.com>
2025-12-16 21:34:00 -0600

    Portfolio-erick - version 56.14 - UI components > index - Features: Importing and exporting switchVariants

--- src/components/ui-components/ui.index.components.js
+++ src/components/ui-components/ui.index.components.js
@@ -1,12 +1,13 @@
 import { loadingVariants } from "./loading.messages.component";
+import { switchVariants } from "./theme.toggle.variant.component";
 
-/**Portfolio-erick - version 56.13 - UI components > index -
+/**Portfolio-erick - version 56.14 - UI components > index -
 * Features:
 
-    --> Importing and exporting 'loadingVariants'
+    --> Importing and exporting 'switchVariants'
 
 * Notes: className has .svg class for this component
 *later will be refacotored
 **/
   
-export { loadingVariants }
+export { loadingVariants, switchVariants }
