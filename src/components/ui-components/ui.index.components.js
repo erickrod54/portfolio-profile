9f02035c7dbc9231f9e40fbe7b5fa6bb5e2ac54a
Erick Rodriguez <erickrod54@gmail.com>
2025-12-19 21:32:00 -0600

    Portfolio-erick - version 56.17 - UI components > index - Features: Importing and exporting Button ui component

--- src/components/ui-components/ui.index.components.js
+++ src/components/ui-components/ui.index.components.js
@@ -1,13 +1,14 @@
+import { ButtonUI } from "./button.ui.component";
 import { loadingVariants } from "./loading.messages.component";
 import { switchVariants } from "./theme.toggle.variant.component";
 
-/**Portfolio-erick - version 56.14 - UI components > index -
+/**Portfolio-erick - version 56.17 - UI components > index -
 * Features:
 
-    --> Importing and exporting 'switchVariants'
+    --> Importing and exporting 'Button' ui component
 
 * Notes: className has .svg class for this component
 *later will be refacotored
 **/
   
-export { loadingVariants, switchVariants }
+export { loadingVariants, switchVariants, ButtonUI }
